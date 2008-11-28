91fd68ef527a790b1ac45111a4d96b66cd89a934
seanm <seanm@d09299c4-db10-0410-bb76-d5d2e039f7b6>
2008-11-28 05:22:12 +0000

    Disable kerneltrap - rss feed seems to be dead.

--- my-user-init.el
+++ my-user-init.el
@@ -11,8 +11,11 @@
 	  ;; ("NewsForge" . "http://newsforge.com/newsforge.rdf")
 	  ("OS news" . "http://www.osnews.com/files/recent.xml")
 	  ;; ("MadPenguin" . "http://www.madpenguin.org/backend.php")
-	  ("Kerneltrap" . "http://kerneltrap.org/node/feed")
+	  ;; ("Kerneltrap" . "http://kerneltrap.org/node/feed")
 	  ;; ("Weather" . "http://www.weatheroffice.gc.ca/rss/city/on-118_e.xml")
 	  ))
     (slashdot-start)
   )
+
+(setq zenirc-server-alist
+      '(("irc.ipv6.oftc.net" nil nil nil nil)))
