b5b681a8f2719649f1494de13afb2f127f9ff88e
seanm <seanm@d09299c4-db10-0410-bb76-d5d2e039f7b6>
2008-11-29 20:46:29 +0000

    Update the slashdot list.

--- my-user-init.el
+++ my-user-init.el
@@ -2,20 +2,7 @@
 (when (and window-system (not running-as-root) (would-like 'slashdot))
   ;; madpenguin.org does not work if User-Agent is Wget
   (setq slashdot-wget-options (append http-wget-options '("-A" "Mozilla")))
-  ;; If you edit this, run (slashdot-load-headings)
-  (setq slashdot-url-alist
-	'(
-	  ("Slashdot" . "http://rss.slashdot.org/Slashdot/slashdot/to")
-	  ;; ("The Register" . "http://www.theregister.co.uk/tonys/slashdot.rdf")
-	  ;; ("Freshmeat" . "http://freshmeat.net/backend/fm.rdf")
-	  ;; ("NewsForge" . "http://newsforge.com/newsforge.rdf")
-	  ("OS news" . "http://www.osnews.com/files/recent.xml")
-	  ;; ("MadPenguin" . "http://www.madpenguin.org/backend.php")
-	  ;; ("Kerneltrap" . "http://kerneltrap.org/node/feed")
-	  ;; ("Weather" . "http://www.weatheroffice.gc.ca/rss/city/on-118_e.xml")
-	  ))
-    (slashdot-start)
-  )
+  (slashdot-start))
 
 (setq zenirc-server-alist
       '(("irc.ipv6.oftc.net" nil nil nil nil)))
