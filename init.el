51cba97e9108db2afdb563dc150bf57a16d7fc96
seanm <seanm@d09299c4-db10-0410-bb76-d5d2e039f7b6>
2009-05-15 23:41:16 +0000

    More ignores.

--- init.el
+++ init.el
@@ -680,7 +680,7 @@ Does the matches case insensitive unless `case-sensitive' is non-nil."
   (setq smerge-diff-excludes
 	'("*.o" "*.obj" "*.a" "*.lib" "*~" ".#*" "CVS" ".svn"
 	  ".git" "*.cmd" "*.lo" "*.ko" ".tmp_versions" "*.Plo"
-	  "modules.order" "*.elc"))
+	  "modules.order" "*.elc" "*.mod.c" "TAGS"))
   (setq smerge-diff-options "-w"))
 
 ;;; -------------------------------------------------------------------------
