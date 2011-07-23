f09b16bde1b625729ee575f0d26ea62d52344ef4
Sean MacLennan <git@seanm.ca>
2011-07-23 01:40:52 -0400

    Now that `my-compile-dir-list' supports svn URLs, we can use them and not rely on gg-dir. gg-dir just did not work well when working on a branch and the trunk at the same time.

--- afore.el
+++ afore.el
@@ -1,55 +1,65 @@
-(defvar gg-dir (expand-file-name "~/goldengate") "* Base goldengate dir")
-(defvar sla-dir nil "* Base sla directory")
 (defvar sla-buf "*sla files*" "* SLA file list buffer")
-(defvar compile-dir-list-was nil)
 
-(eval-when-compile (require 'etags))
+(defvar gg-svn-url "svn://localhost/gg") ;; Sandbox
+;;(defvar gg-svn-url "svn://redbox/repo/projects/goldengate")
 
 (add-to-list 'my-compile-dir-list '(".*/LM2.[0-9.]+/") t)
 
-(defun set-gg-dir (dir)
-  (interactive "DDir: ")
-  (setq gg-dir (my-expand-dir-name dir))
-  (setq sla-dir (concat gg-dir "/sla/"))
+;; throughput engine must be before sla proper
+(add-to-list 'my-compile-dir-list
+	     (list (concat gg-svn-url "/.*/sla/throughput-engine/")
+		   "throughputengine" 'sla-build) t)
+(add-to-list 'my-compile-dir-list
+	     (list (concat gg-svn-url "/.*/sla/")
+		   "sla" 'sla-build) t)
 
-  ;; Reset my-compile-dir-list
-  (if compile-dir-list-was
-      (setq my-compile-dir-list compile-dir-list-was)
-    (setq compile-dir-list-was my-compile-dir-list))
+;; Sighhh.... vpn source uses 2 and 3 and sometimes 4
+(add-to-list 'my-compile-dir-list
+	     (list (concat gg-svn-url "/.*/vpn/src/") nil 'space-indent-2) t)
 
-  ;; Sighhh.... vpn source uses 2 and 3 and sometimes 4
-  (add-to-list 'my-compile-dir-list
-	       (list (concat gg-dir "/vpn/src/") nil 'space-indent-2) t)
-  (add-to-list 'my-compile-dir-list
-	       (list (concat gg-dir "/util/vnodecom/") nil 'space-indent-4) t)
-  ;; throughput engine must be before sla proper
-  (add-to-list 'my-compile-dir-list (list (concat sla-dir "throughput-engine/")
-					  "throughputengine" 'sla-build) t)
-  (add-to-list 'my-compile-dir-list (list sla-dir "sla" 'sla-build) t)
-  )
+(add-to-list 'my-compile-dir-list
+	     (list (concat gg-svn-url "/.*/util/vnodecom/") nil 'space-indent-4) t)
+
+;; *****
+;; etags and compile support
+
+(eval-when-compile (require 'etags))
+
+(defvar sla-dir nil "SLA directory. Set in `sla-build'.")
+(make-variable-buffer-local 'sla-dir)
 
 (defun sla-etags (&optional force)
   (interactive "P")
+  (unless sla-dir (error "No SLA directory associated with this buffer."))
   ;; SAM If sla-img older than TAGS, drop out?
   ;; SAM How do we decide when to rebuild sla-buf?
   (when force (kill-buffer sla-buf))
   (my-etag-tree sla-dir sla-buf))
 
 (defun sla-build (dir arg)
-  "sla is weird in that it must be built from the directory *above* sla"
+  "This should be called via `my-compile-dir-list'."
+
+  ;; Correct for throughputengine directory
+  (when (string= arg "throughputengine")
+      (setq dir (expand-file-name (concat dir "/.."))))
 
   (tab-indent-2 dir arg)
 
-  (setq buffer-tag-table (concat gg-dir "/sla"))
+  ;; etags
+  (setq sla-dir (concat dir "/"))
+  (setq buffer-tag-table dir)
   (add-hook 'my-compile-after-hooks 'sla-etags)
 
-  ;; Note that compile-command will already be buffer local
-  (setq compile-command (concat "make -C " gg-dir " " arg))
+  ;; sla is weird in that it must be built from the directory *above* sla
+  (let ((top-dir (expand-file-name (concat dir "/.."))))
 
-  (set (make-local-variable 'make-clean-command)
-       (concat "make -C " gg-dir " " arg "-clean " arg)))
+    ;; Note that compile-command will already be buffer local
+    (setq compile-command (concat "make -C " top-dir " " arg))
 
-(set-gg-dir gg-dir)
+    (set (make-local-variable 'make-clean-command)
+	 (concat "make -C " top-dir " " arg "-clean " arg))))
+
+;; *****
 
 ;; Codebase contains too much bogus whitespace
 (whitespace-global-mode 0)
