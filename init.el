204d36ff62b08513951c212ea62f6f77759f4231
Sean MacLennan <git@seanm.ca>
2012-12-10 21:15:44 -0500

    Move oo-browser code to obsolete.

--- init.el
+++ init.el
@@ -1420,22 +1420,6 @@ We ignore the 3rd number."
        (setq auto-mode-alist
 	     (cons '("\\.pdf$" . do-acroread) auto-mode-alist))))
 
-;;; ----------------------------------------------
-;; oo-browser
-
-(defun oo-browser-start ()
-  (interactive)
-  (my-package-cond
-   (oo-browser
-    (dolist (path load-path)
-      (if (string-match "/oo-browser/$" path)
-	  (append-to-list 'load-path (concat path "hypb/"))))
-    (require 'br-start)
-    (global-set-key "\C-c\C-o" 'oo-browser)
-    (oo-browser))
-   (t (error "oo-browser not installed."))))
-(global-set-key "\C-c\C-o" 'oo-browser-start)
-
 ;;}}}
 
 ;;{{{ Mail
