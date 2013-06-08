6f980c51b25b24cb2e7dc150a2c7500d5782f71b
Sean MacLennan <git@seanm.ca>
2013-06-07 22:52:43 -0400

    I haven't used loud-compile in decades... move to obsolete.

--- init.el
+++ init.el
@@ -67,11 +67,6 @@
 	     nil))
    (emacs (would-like package no-list))))
 
-(defvar have-sound
-  (and (fboundp 'device-sound-enabled-p)
-       (device-sound-enabled-p))
-  "* Non-nil if sound is enabled. XEmacs defaults this correctly, GNU Emacs cannot.")
-
 (if (would-like 'rcfiles)
     (rcfiles-register-rc-files)
   (setq rcfiles-directory (concat dot-dir "rc")))
@@ -719,41 +714,6 @@ If `compilation-ask-about-save' is nil, saves the file without asking."
     (make-local-variable 'compile-command)
     (setq compile-command cmd)
     (my-do-compile compile-command)))
-
-;;; -------------------------------------------------------------------------
-;; Audible compilation completion
-(defvar loud-compile    nil   "* If t, `ding' when compile finished.")
-
-(defun loud-finish (buff exit)
-  "If `loud-compile', `ding'. Assign to `compilation-finish-function'."
-  (and loud-compile
-       (not (string= exit "finished\n"))
-       (ding)))
-(my-feature-cond
- (xemacs (setq compilation-finish-function 'loud-finish))
- (t (add-to-list 'compilation-finish-functions 'loud-finish)))
-
-(my-feature-cond
- (xemacs
-  ;; Note: XEmacs 21.5 will ding the visible bell if this funciton
-  ;; returns nil, which it will on a failure.
-  (defun loud-finish-fancy (buff exit)
-    "If `loud-compile', `ding'. Assign to `compilation-finish-function'."
-    (when loud-compile
-      (let ((visible-bell nil))
-	(if (string= exit "finished\n")
-	    (ding nil 'compile-ok)
-	  (ding nil 'compile-failed)))))
-
-  (when have-sound
-    (condition-case nil
-	(progn
-	  (load-sound-file "YouTheMan.au" 'compile-ok)
-	  (load-sound-file "Snicker.au" 'compile-failed)
-	  (setq compilation-finish-function 'loud-finish-fancy)
-	  (setq loud-compile t))
-      (error
-       (push "Sound" would-have-liked-list))))))
 
 ;;----------------------------------------------------------------
 (defvar make-clean-command "make clean all"
