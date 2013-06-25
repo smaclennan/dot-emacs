ffca8cd4a0224ccdee71dbae23c58a84c38180a8
Sean MacLennan <git@seanm.ca>
2013-06-24 22:25:41 -0400

    Add smerge rc file.

--- init.el
+++ init.el
@@ -551,13 +551,6 @@ Does the matches case insensitive unless `case-sensitive' is non-nil."
     (goto-char mark))
   (add-local-compile-command nil))
 
-(when (would-like 'smerge)
-  (setq smerge-diff-excludes
-	'("*.o" "*.obj" "*.a" "*.lib" "*~" ".#*" "CVS" ".svn"
-	  ".git" "*.cmd" "*.lo" "*.ko" ".tmp_versions" "*.Plo"
-	  "modules.order" "*.elc" "*.mod.c" "TAGS"))
-  (setq smerge-diff-options "-w"))
-
 (would-like 'my-c-tools)
 
 ;;; -------------------------------------------------------------------------
---
+++ rc/smerge-rc.el
@@ -0,0 +1,6 @@
+(setq smerge-diff-excludes
+	'("*.o" "*.obj" "*.a" "*.lib" "*~" ".#*" "CVS" ".svn"
+	  ".git" "*.cmd" "*.lo" "*.ko" ".tmp_versions" "*.Plo"
+	  "modules.order" "*.elc" "*.mod.c" "TAGS"))
+
+(setq smerge-diff-options "-w")
