c249d058c5159e910ab7792b15462bd903cd91ad
Sean MacLennan <git@seanm.ca>
2013-06-25 02:05:28 -0400

    madpenguin.org is long gone. RIP.

--- my-user-init.el
+++ my-user-init.el
@@ -1,5 +1,3 @@
 ;; Start *after* display-time
 (when (and window-system (not running-as-root) (would-like 'slashdot))
-  ;; madpenguin.org does not work if User-Agent is Wget
-  (setq slashdot-wget-options (append http-wget-options '("-A" "Mozilla")))
   (slashdot-start))
