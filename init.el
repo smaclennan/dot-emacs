53b3e2ceefcd4749174fc4d0b886399d885d8cf7
Sean MacLennan <git@seanm.ca>
2013-08-09 00:29:15 -0400

    Can start with NO XEmacs packages.

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;; S?X?Emacs setup -*- Mode:emacs-lisp -*-
 ;; This file should work with XEmacs 2x.x, Emacs 21.x, or SXEmacs
 
-;; Assumes at least the xemacs-base package
+;; Aug 9, 2013: Can run with no packages.
 
 ;;{{{ Configuration variables / functions
 
@@ -332,40 +332,7 @@ If `compilation-ask-about-save' is nil, saves the file without asking."
 
 ;;; -------------------------------------------------------------------------
 ;; compile variables
-(setq compilation-read-command   nil
-      compilation-ask-about-save nil
-      compilation-window-height  12
-      compilation-error-regexp-systems-list '(gnu)
-      compile-command "make ")
-
-;; This gives the compilation buffer its own frame
-;;(push "*compilation*" special-display-buffer-names)
-
-(defun my-do-compile (cmd)
-  (save-some-buffers (not compilation-ask-about-save) nil)
-  (my-feature-cond
-   (xemacs (compile-internal cmd "No more errors"))
-   (emacs  (compilation-start cmd))))
-
-(defun my-set-compile ()
-  (interactive)
-  (require 'compile)
-  (let ((cmd (read-string "Compile Command: " compile-command)))
-    (make-local-variable 'compile-command)
-    (setq compile-command cmd)
-    (my-do-compile compile-command)))
-
-;;----------------------------------------------------------------
-(defvar make-clean-command "make clean all"
-  "*Command used by the `make-clean' function.")
-
-(defun make-clean (&optional arg)
-  "Run a make clean."
-  (interactive "P")
-  (require 'compile) ;; needed for compile-internal
-  (if arg
-      (setq make-clean-command (read-string "Command: " make-clean-command)))
-  (my-do-compile make-clean-command))
+(would-like 'compile) ; xemacs-base
 
 ;;; -------------------------------------------------------------------------
 ;; hide-copyleft
---
+++ rc/compile-rc.el
@@ -0,0 +1,32 @@
+(setq compilation-read-command   nil
+      compilation-ask-about-save nil
+      compilation-window-height  12
+      compilation-error-regexp-systems-list '(gnu)
+      compile-command "make ")
+
+;; This gives the compilation buffer its own frame
+;;(push "*compilation*" special-display-buffer-names)
+
+(defun my-do-compile (cmd)
+  (save-some-buffers (not compilation-ask-about-save) nil)
+  (my-feature-cond
+   (xemacs (compile-internal cmd "No more errors"))
+   (emacs  (compilation-start cmd))))
+
+(defun my-set-compile ()
+  (interactive)
+  (let ((cmd (read-string "Compile Command: " compile-command)))
+    (make-local-variable 'compile-command)
+    (setq compile-command cmd)
+    (my-do-compile compile-command)))
+
+;;----------------------------------------------------------------
+(defvar make-clean-command "make clean all"
+  "*Command used by the `make-clean' function.")
+
+(defun make-clean (&optional arg)
+  "Run a make clean."
+  (interactive "P")
+  (if arg
+      (setq make-clean-command (read-string "Command: " make-clean-command)))
+  (my-do-compile make-clean-command))
