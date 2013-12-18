fd0dc13f59441fb582890f3fefa5632a79e7422d
Sean MacLennan <git@seanm.ca>
2013-12-18 00:43:01 -0500

    Seems to be working better. Let's give the new modeline a second chance.

--- rc/xemacs-21.5.el
+++ rc/xemacs-21.5.el
@@ -3,6 +3,6 @@
       default-buffer-file-coding-system  'raw-text)
 
 ;; New modeline format does not fit on a smaller window
-(setq-default
- modeline-buffer-id
- (list (cons modeline-buffer-id-left-extent 'modeline-buffer-id-left)))
+;(setq-default
+; modeline-buffer-id
+; (list (cons modeline-buffer-id-left-extent 'modeline-buffer-id-left)))
