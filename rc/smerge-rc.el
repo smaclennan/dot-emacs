44991d199dd30ffb402ed1e08da2c9a23563f294
Sean MacLennan <smaclennan@diablo-technologies.com>
2016-04-07 10:42:12 -0400

    Ignoring .builtin saves a few hundred files on kernel diffs.

--- rc/smerge-rc.el
+++ rc/smerge-rc.el
@@ -1,6 +1,6 @@
 (setq smerge-diff-excludes
 	'("*.o" "*.obj" "*.a" "*.lib" "*~" ".#*" "CVS" ".svn"
 	  ".git" "*.cmd" "*.lo" "*.ko" ".tmp_versions" "*.Plo"
-	  "modules.order" "*.elc" "*.mod.c" "TAGS"))
+	  "modules.order" "*.elc" "*.mod.c" "TAGS" "*.builtin"))
 
 (setq smerge-diff-options "-w")
