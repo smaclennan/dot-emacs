b22f34ceb157c09ca000d25a59b5c3ae679f41c6
Sean MacLennan <seanm@seanm.ca>
2016-05-07 14:57:01 -0400

    A good example of how to handle a directory with lots of sub-directories  and a mix of coding styles.
    One caveat: using one tags file for all the sub-directories works
    because the developers where careful with namespaces.

--- work.el
+++ work.el
@@ -3,9 +3,47 @@
 (add-to-list 'my-compile-dir-list
 	     (list (expand-file-name "~/linux-kernel/") make-j "linux") t)
 
-(add-to-list 'my-compile-dir-list
-	     (list ".*?/drv/" make-j "linux") t)
-
 ;; Use the non-greedy match so include/qemu works
 (add-to-list 'my-compile-dir-list
 	     (list ".*?/qemu/" make-j 'space-indent-4) t)
+
+(require 'etags)
+
+(defvar bf-dir nil "Interval variable.")
+(make-variable-buffer-local 'bf-dir)
+
+(defvar bf-app-dirs '("barf" "bfmgr" "dmxmgr")
+  "* List of application sub-directories.")
+
+(defun bf-tags ()
+  (when bf-dir
+    (let ((buf (get-buffer-create "*bf tags *")))
+      ;; Emacs does not have (erase-buffer buf) :(
+      (save-current-buffer (set-buffer buf) (erase-buffer))
+      (my-tag-tree bf-dir buf))))
+
+(defun bf-func (matched-dir target)
+  (let ((dir (buffer-file-name)) subdir)
+    ;; Get the subdir under bart's fault
+    (when (string-match "/barts-fault/\\([^/]+\\)/" dir)
+      (setq subdir (substring dir (match-beginning 1) (match-end 1))))
+
+    ;; Set driver directories to Linux style and apps to spaces
+    (if (member subdir bf-app-dirs)
+	(setq c-basic-offset 4 tab-width 4 indent-tabs-mode nil)
+      ;; I set tab-width in my-c-mode-common-hook. Reset it here.
+      (kill-local-variable 'tab-width)
+      (c-set-style "linux"))
+
+    ;; Use one global tagfile
+    (make-local-variable 'tags-file-name)
+    (setq tags-file-name (concat matched-dir "TAGS"))
+
+    ;; Setup for etags
+    (setq bf-dir matched-dir)
+    (add-hook 'after-save-hook 'bf-tags)
+    ))
+
+(add-to-list 'my-compile-dir-list (list ".*?/barts-fault/" make-j 'bf-func) t)
+
+(add-to-list 'my-compile-dir-list (list ".*?/drv/" make-j "linux") t)
