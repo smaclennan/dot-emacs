0fe94599f5e9cd2f6307d6f62818c0342287217a
Sean MacLennan <seanm@seanm.ca>
2016-06-17 21:31:23 -0400

    Port to Emacs 23 for Centos 6.

--- esp/build-loaddefs.el
+++ esp/build-loaddefs.el
@@ -1,5 +1,6 @@
 (eval-when-compile (require 'cl))
 
+;; Emacs 23.1 does not support outfile arg to `update-file-autoloads'
 (defun build-loaddefs (&optional dir loaddefs)
   (unless dir
     (setq dir (pwd))
@@ -7,5 +8,6 @@
     (setq dir (match-string 1 dir)))
   (unless loaddefs
     (setq loaddefs (concat dir "/" (file-name-nondirectory dir) "-loaddefs.el")))
-  (loop for file in (directory-files dir t "^[^._].*\.el$") do
-	(update-file-autoloads file t loaddefs)))
+  (let ((generated-autoload-file loaddefs))
+    (loop for file in (directory-files dir t "^[^._].*\.el$") do
+	  (update-file-autoloads file t))))
