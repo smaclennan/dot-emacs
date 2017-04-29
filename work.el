c5b485dfd6af9eafb27d83a6e54293bcbd1f90c7
Sean MacLennan <seanm@seanm.ca>
2017-04-29 13:28:58 -0400

    Cleanup the compile rules.

--- work.el
+++ work.el
@@ -1,18 +1,8 @@
 (require 'my-compile)
 (require 'my-tags)
 
-(add-to-list 'my-compile-dir-list
-	     (list (expand-file-name "~/linux-kernel/") make-j "linux") t)
-
-(defun qemu-func (matched-dir target)
-  (space-indent-4 matched-dir target)
-  (my-tag-dirs-helper matched-dir "*qemu tags*"))
-
-;; Use the non-greedy match so include/qemu works
-(add-to-list 'my-compile-dir-list
-	     (list ".*?/qemu/" make-j 'qemu-func) t)
-
-(defvar bf-app-dirs '("barf" "bfmgr" "dmxmgr")
+;; Bart's Fault
+(defvar bf-app-dirs '("barf" "bfmgr" "dmxmgr" "lib")
   "* List of application sub-directories.")
 
 (defun bf-func (matched-dir target)
@@ -33,21 +23,28 @@
      "*bf tags*")
     ))
 
-(defun bf-guest-func (matched-dir target)
-  (if (string-match "/drv/" (buffer-file-name))
-      (progn
-	;; Set driver directories to Linux style and apps to spaces
-	;; I set tab-width in my-c-mode-common-hook. Reset it here.
-	(kill-local-variable 'tab-width)
-	(c-set-style "linux"))
-    (setq compile-command "make ")
-    (setq c-basic-offset 4 tab-width 4 indent-tabs-mode nil))
+(defun dmx-simple-func (matched-dir target)
+      ;; Linux style
+      (kill-local-variable 'tab-width)
+      (c-set-style "linux")
 
-  (add-hook 'after-save-hook 'my-tag-simple))
+      (add-hook 'after-save-hook 'my-tag-simple))
 
-;; Bart's Fault
 (add-to-list 'my-compile-dir-list (list ".*?/barts-fault/drv/" make-j 'bf-func) t)
-(add-to-list 'my-compile-dir-list (list ".*?/barts-fault/guest/" nil 'bf-guest-func) t)
+(add-to-list 'my-compile-dir-list (list ".*?/barts-fault/dmx-trap/" make-j 'dmx-simple-func) t)
+(add-to-list 'my-compile-dir-list (list ".*?/barts-fault/guest/drv/" make-j 'dmx-simple-func) t)
 (add-to-list 'my-compile-dir-list '(".*?/barts-fault/" nil bf-func) t)
 
-(add-to-list 'my-compile-dir-list '(".*?/drv/" make-j "linux") t)
+;; 2.x kernel
+(add-to-list 'my-compile-dir-list
+	     (list (expand-file-name "~/linux-kernel/") make-j "linux") t)
+
+;; qemu
+(defun qemu-func (matched-dir target)
+  (space-indent-4 matched-dir target)
+  (my-tag-dirs-helper matched-dir "*qemu tags*"))
+
+;; Use the non-greedy match so include/qemu works
+(add-to-list 'my-compile-dir-list
+	     (list ".*?/qemu/" make-j 'qemu-func) t)
+
