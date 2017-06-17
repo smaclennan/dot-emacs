d8a5c478d611f73a19cec15e10819feb52af0c6b
Sean MacLennan <smaclennan@diablo-technologies.com>
2017-06-16 22:47:10 -0400

    Switch to Linux only C style.

--- work.el
+++ work.el
@@ -2,33 +2,21 @@
 (require 'my-tags)
 (require 'cc-mode)
 
+(add-to-list
+ 'copylefts-to-hide
+ '("Copyright (c) [0-9]+ Diablo Technologies Inc\\." . "\\*/"))
+
 ;; Bart's Fault
-(defvar bf-app-dirs '("barf" "bfmgr" "dmxmgr" "lib")
-  "* List of application sub-directories.")
-
-(defun bf-func (matched-dir target)
-  (let ((dir (buffer-file-name)) subdir)
-    ;; Get the subdir under bart's fault
-    (when (string-match "/barts-fault/\\([^/]+\\)/" dir)
-      (setq subdir (substring dir (match-beginning 1) (match-end 1))))
-
-    ;; Set driver directories to Linux style and apps to spaces
-    (if (member subdir bf-app-dirs)
-	(setq c-basic-offset 4 tab-width 4 indent-tabs-mode nil)
-      ;; I set tab-width in my-c-mode-common-hook. Reset it here.
-      (kill-local-variable 'tab-width)
-      (c-set-style "linux"))
-
-    (my-tag-dirs-helper
-     (replace-regexp-in-string "/barts-fault/.*" "/barts-fault/" matched-dir)
-     "*bf tags*")
-    ))
-
-(defun dmx-simple-func (matched-dir target)
+(defun dmx-simple-func (matched-dir target &optional buf)
       ;; Linux style
       (kill-local-variable 'tab-width)
       (c-set-style "linux")
-      (my-tag-dirs-helper matched-dir))
+      (my-tag-dirs-helper matched-dir buf))
+
+(defun bf-func (matched-dir target)
+  (dmx-simple-func
+   (replace-regexp-in-string "/barts-fault/.*" "/barts-fault/" matched-dir)
+   "*bf tags*"))
 
 (add-to-list 'my-compile-dir-list (list ".*?/barts-fault/drv/" make-j 'bf-func) t)
 (add-to-list 'my-compile-dir-list (list ".*?/barts-fault/dmx-trap/" make-j 'dmx-simple-func) t)
@@ -47,4 +35,3 @@
 ;; Use the non-greedy match so include/qemu works
 (add-to-list 'my-compile-dir-list
 	     (list ".*?/qemu/" make-j 'qemu-func) t)
-
