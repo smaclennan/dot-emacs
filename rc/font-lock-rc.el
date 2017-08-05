330f45771b4fda778a4f1410e28c0f12226a8b19
Sean MacLennan <seanm@seanm.ca>
2017-08-05 15:28:35 -0400

    Emacs calls font-lock before window-config, so call window-config from font-lock if necessary to set laptop-mode. Move font-lock-comment-warn-face definition before the laptop-mode code so we can make it non-bold too.

--- rc/font-lock-rc.el
+++ rc/font-lock-rc.el
@@ -5,25 +5,6 @@
 	      font-lock-verbose nil
 	      font-lock-maximum-size nil)
 
-;; Change a couple of faces
-(make-face-bold 'font-lock-function-name-face)
-(set-face-foreground 'font-lock-function-name-face "blue")
-;; Um, this is the default
-;(set-face-foreground 'font-lock-string-face "green4")
-(if window-system
-    (progn
-      (set-face-foreground 'font-lock-comment-face "FireBrick")
-      ;; In my init.el, laptop mode is always set before font-lock
-      (when laptop-mode
-	(dolist (face '(font-lock-function-name-face font-lock-warning-face))
-	  (set-face-font face laptop-mode-font))))
-  ;; Consoles have less colors to play with
-  (set-face-foreground 'font-lock-comment-face "red")
-  (set-face-foreground 'font-lock-string-face "green")
-  (set-face-foreground 'font-lock-keyword-face "blue")
-  (set-face-foreground 'font-lock-variable-name-face "purple")
-  )
-
 ;; -------------------------------------------------------------------------
 ;; font-lock-comment-warn
 ;; I want all comments with my initials (SAM) at the start to be very bold
@@ -36,6 +17,31 @@
     (t (:bold t)))
   "Font Lock mode face used to highlight warning comments."
   :group 'font-lock-faces)
+
+;; Change a couple of faces
+(make-face-bold 'font-lock-function-name-face)
+(set-face-foreground 'font-lock-function-name-face "blue")
+;; Um, this is the default
+;(set-face-foreground 'font-lock-string-face "green4")
+
+;; Emacs calls font-lock before window-config
+(unless (boundp 'laptop-mode)
+  (load (concat dot-dir "rc/window-config")))
+
+(if window-system
+    (progn
+      (set-face-foreground 'font-lock-comment-face "FireBrick")
+      (when laptop-mode
+	(dolist (face '(font-lock-function-name-face
+			font-lock-comment-warn-face
+			font-lock-warning-face))
+	  (set-face-font face laptop-mode-font))))
+  ;; Consoles have less colors to play with
+  (set-face-foreground 'font-lock-comment-face "red")
+  (set-face-foreground 'font-lock-string-face "green")
+  (set-face-foreground 'font-lock-keyword-face "blue")
+  (set-face-foreground 'font-lock-variable-name-face "purple")
+  )
 
 (if running-xemacs
     (progn
