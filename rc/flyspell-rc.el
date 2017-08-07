ba37f82502cd24f2a8b80c0ccbce7d380f73563c
Sean MacLennan <seanm@seanm.ca>
2017-08-07 16:02:34 -0400

    flyspell face fixups only needed for XEmacs.

--- rc/flyspell-rc.el
+++ rc/flyspell-rc.el
@@ -1,3 +1,5 @@
-(when laptop-mode
-  (set-face-font 'flyspell-incorrect-face laptop-mode-font)
-  (set-face-font 'flyspell-duplicate-face laptop-mode-font))
+(my-feature-cond
+  (xemacs
+   (when laptop-mode
+     (set-face-font 'flyspell-incorrect-face laptop-mode-font)
+     (set-face-font 'flyspell-duplicate-face laptop-mode-font))))
