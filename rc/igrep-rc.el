3cfc7757aa467feeab08dbd8312cee4d41920f92
Sean MacLennan <seanm@seanm.ca>
2017-08-31 13:10:22 -0400

    Compiler warning cleanups.

--- rc/igrep-rc.el
+++ rc/igrep-rc.el
@@ -2,17 +2,24 @@
 (put 'igrep-files-default 'c-mode (lambda () "*.[ch]"))
 (put 'igrep-files-default 'emacs-lisp-mode (lambda () "*.el"))
 
+(defun igrep-set-finish-function (func)
+  (my-feature-cond
+    (compilation-finish-functions
+     (setq compilation-finish-functions func))
+    (t
+     (setq compilation-finish-function func))))
+
 (defadvice igrep (before windowize activate)
   "This removes a final false match from `igrep' on the finished
 line with `next-error'."
-  (setq compilation-finish-function
-	'(lambda (buf status)
-	   (save-excursion
-	     (set-buffer buf)
-	     (save-excursion
-	       (goto-char (point-min))
-	       ;; Emacs has a "started" line and will have text
-	       ;; after "finished" and before "at".
-	       (while (re-search-forward "^Igrep \\(started\\|finished\\) .*$" nil t)
-		 (replace-match ""))))
-	   (setq compilation-finish-function nil))))
+  (igrep-set-finish-function
+   '(lambda (buf status)
+      (save-excursion
+	(set-buffer buf)
+	(save-excursion
+	  (goto-char (point-min))
+	  ;; Emacs has a "started" line and will have text
+	  ;; after "finished" and before "at".
+	  (while (re-search-forward "^Igrep \\(started\\|finished\\) .*$" nil t)
+	    (replace-match ""))))
+      (igrep-set-finish-function nil))))
