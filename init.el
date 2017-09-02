ff0e29b802d302661a29aa6a92a24e4c96c30184
Sean MacLennan <seanm@seanm.ca>
2017-09-02 13:13:02 -0400

    Don't duplicate the sam-common code.

--- init.el
+++ init.el
@@ -60,27 +60,7 @@
 	nil)
     (would-like package no-list)))
 
-;; GNU emacs sets emacs
-;; XEmacs sets xemacs
-;; SXEmacs sets sxemacs and xemacs
-(defmacro my-feature-cond (&rest clauses)
-  "Test CLAUSES for feature, variable, or function at compile time.
-Each clause is (FEATURE BODY...)."
-  (dolist (x clauses)
-    (let ((feature (car x))
-	  (body (cdr x)))
-      (when (or (eq feature t)
-		(featurep feature)
-		(fboundp feature)
-		(boundp feature))
-	(return (cons 'progn body))))))
-(put 'my-feature-cond 'lisp-indent-hook 'defun)
-
-(my-feature-cond
-  (xemacs ;; Must be a defalias for my-isearch-word-forward
-   (defalias 'my-interactive-p 'interactive-p))
-  (t ;; Must be a macro to work
-   (defmacro my-interactive-p () `(called-interactively-p 'interactive))))
+(require 'sam-common)
 
 ;; Split the system-name up into host and domain name.
 ;; We need this up front for sendmail-rc.
