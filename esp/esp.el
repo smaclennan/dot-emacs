818dfd538a7ca314783d1f0daf3057412dde766f
Sean MacLennan <seanm@seanm.ca>
2017-12-26 13:44:48 -0500

    No end of grief with push-tag-mark... go back to old version.

--- esp/esp.el
+++ esp/esp.el
@@ -132,10 +132,7 @@ instead, uses tag around or before point."
 		(find-tag-tag "Find tag: "))
 	    (find-tag (find-tag-default))))
 
-(defun push-tag-mark ()
-  (my-feature-cond
-    (xref-push-marker-stack (xref-push-marker-stack))
-    (t (ring-insert find-tag-marker-ring (point-marker)))))
+(defun push-tag-mark () (ring-insert find-tag-marker-ring (point-marker)))
 
 (defalias 'kill-entire-line 'kill-whole-line)
 
