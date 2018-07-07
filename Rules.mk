504076a441dbddbab4b6c1e20c5ef31e0c347615
Sean MacLennan <seanm@seanm.ca>
2018-07-07 12:34:11 -0400

    Move more rules to the top level Rules.mk

--- Rules.mk
+++ Rules.mk
@@ -1,9 +1,14 @@
 .SUFFIXES: .el .elc
 
+first_rule: all
+
 # When building inside of Emacs it seems EMACS=t
 ifeq ($(EMACS),t)
 EMACS=
 endif
+
+DIR := $(shell pwd)
+SUBDIR := $(shell basename $(DIR))
 
 ifeq ($(EMACS),)
 ifneq ($(findstring .xemacs, $(DIR)),)
@@ -16,3 +21,15 @@ EMACS=emacs
 endif
 endif
 endif
+
+ifeq ($(EMACS), emacs)
+HELPER := -l ~/.emacs.d/esp/esp
+
+$(SUBDIR)-loaddefs.el:
+	@echo Create $(EMACS) $(SUBDIR)-loaddefs.el ...
+	@$(EMACS) -batch -q $(HELPER) -l build-loaddefs.el -f build-loaddefs
+endif
+
+.el.elc:
+	$(EMACS) -batch -q $(HELPER) -f batch-byte-compile $<
+	@rm -f $(SUBDIR)-loaddefs.el
