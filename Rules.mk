105816304826c630345a7bb2100e5c7f616386c4
Sean MacLennan <seanm@seanm.ca>
2018-07-10 18:34:09 -0400

    Move the XEmacs rules to the top level Rules.mk

--- Rules.mk
+++ Rules.mk
@@ -3,13 +3,13 @@
 
 first-rule: all
 
+DIR := $(shell pwd)
+SUBDIR := $(shell basename $(DIR))
+
 # When building inside of Emacs it seems EMACS=t
 ifeq ($(EMACS),t)
 EMACS=
 endif
-
-DIR := $(shell pwd)
-SUBDIR := $(shell basename $(DIR))
 
 ifeq ($(EMACS),)
 ifneq ($(findstring .xemacs, $(DIR)),)
@@ -23,14 +23,42 @@ endif
 endif
 endif
 
-ifeq ($(EMACS), emacs)
+ifeq ($(findstring xemacs,$(EMACS)),)
+# GNU Emacs
+
 HELPER := -l ~/.emacs.d/esp/esp
 
 $(SUBDIR)-loaddefs.el:
 	@echo Create $(EMACS) $(SUBDIR)-loaddefs.el ...
 	@$(EMACS) -batch -q $(HELPER) -l build-loaddefs.el -f build-loaddefs
+
+LOAD_FILES=$(SUBDIR)-loaddefs.el
+
+else
+# XEmacs
+
+VERSION := $(shell $(EMACS) -V | cut -d' ' -f2)
+
+auto-autoloads.el:
+ifeq ($(EMACS),sxemacs)
+	$(EMACS) -batch -f batch-create-autoloads .
+else
+  ifeq ($(VERSION),21.4)
+	$(EMACS) -batch -q -no-site-file -l autoload \
+		-f batch-update-directory .
+  else
+	$(EMACS) -batch -q -no-site-file -l autoload \
+		-f batch-update-directory-autoloads $(SUBDIR) .
+  endif
+endif
+
+custom-load.el: $(LISP)
+	$(EMACS) -batch -vanilla -l cus-dep -f Custom-make-dependencies .
+
+LOAD_FILES=auto-autoloads.el custom-load.el
+
 endif
 
 .el.elc:
 	$(EMACS) -batch -q $(HELPER) -f batch-byte-compile $<
-	@rm -f $(SUBDIR)-loaddefs.el auto-autoloads.el
+	@rm -f $(LOAD_FILES)
