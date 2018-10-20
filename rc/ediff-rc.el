3954fe119d2f8a4517aeb408be7f4b4faeb6f459
Sean MacLennan <seanm@seanm.ca>
2018-10-20 12:56:56 -0400

    ediff seems to want to leave the bottom buffer selected, but I want the top buffer selected. Move the selected window to the top window.

--- rc/ediff-rc.el
+++ rc/ediff-rc.el
@@ -25,6 +25,14 @@
     (my-set-face 'ediff-even-diff-B    "black" "white" 'highlight)
     ))
 
+(defun my-ediff-quit ()
+  "Ediff seems to always leave the bottom buffer selected but I
+want the top buffer."
+  (let ((top-window (frame-first-window (selected-frame))))
+    (while (not (eq (selected-window) top-window))
+      (other-window 1))))
+(add-hook 'ediff-quit-hook 'my-ediff-quit t)
+
 ;; -------------------
 (when (not running-xemacs)
   ;; Hack to put the ediff control in the right place. If we don't do
