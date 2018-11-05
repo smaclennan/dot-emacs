e2edf44cf7ab00e8c821e9007d1b3dd442277562
Sean MacLennan <seanm@seanm.ca>
2018-11-05 15:54:21 -0500

    Let's see if we can compile C and Lisp at the same time

--- rc/lisp-mode-rc.el
+++ rc/lisp-mode-rc.el
@@ -3,6 +3,8 @@
       (file-exists-p "makefile")
       (file-exists-p "GNUmakefile")))
 
+(defvar my-byte-compile-buffer-called nil "Never set this")
+
 (defun my-byte-compile-buffer (ignore-makefile)
   "If a makefile exists in the current directory, call make. Always byte
 compile and load the current buffer. With a prefix arg do not check
@@ -14,11 +16,21 @@ asking."
   (require 'compile)
   (save-some-buffers (not compilation-ask-about-save))
   (when (and (not ignore-makefile) (makefile-exists-p))
-    (compile compile-command))
+    (let ((my-byte-compile-buffer-called t))
+      (compile compile-command)))
   (emacs-lisp-byte-compile-and-load))
 
 ;; Redefine compile
 (define-key emacs-lisp-mode-map [f7] 'my-byte-compile-buffer)
+
+(defun gen-compilation-name (mode)
+  "Rename the compilation buffer if compiling lisp code. This
+allows lisp code to be compiled while doing another compile."
+  (if my-byte-compile-buffer-called
+      "*lisp-compile*"
+    ;; This is the default
+    (concat "*" (downcase mode) "*")))
+(setq compilation-buffer-name-function 'gen-compilation-name)
 
 ;; greedy-delete
 (when (would-like 'greedy-delete)
