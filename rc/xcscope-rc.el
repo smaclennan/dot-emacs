484130e45cbbf5d4efac6ecb926a5b088719e6ba
Sean MacLennan <seanm@seanm.ca>
2018-12-01 13:46:30 -0500

    With my-cscope, I don't need this

--- rc/xcscope-rc.el
+++
@@ -1,7 +0,0 @@
-
-;; Everybody uses mouse-1... except xcscope
-(define-key cscope-list-entry-keymap [mouse-1]
-  'cscope-mouse-select-entry-other-window)
-
-;; I like speed
-(setq cscope-command-args "-q")
