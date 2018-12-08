0ac04d24dd3780ff8d974c7b4fa2eda79dc3dde5
Sean MacLennan <seanm@seanm.ca>
2018-12-07 19:37:18 -0500

    Update appointments when diary changes.

--- rc/appt-rc.el
+++ rc/appt-rc.el
@@ -1,2 +1,15 @@
 ;; Use (diary) to display it
 (setq appt-display-diary nil)
+
+;; We need the full path for matching
+(setq diary-file (expand-file-name diary-file))
+
+;; Update appt if the diary file changed
+(defun check-if-diary-changed ()
+  (when (equal (buffer-file-name) diary-file)
+    (appt-check t)))
+(add-hook 'after-revert-hook 'check-if-diary-changed)
+
+;; Enable auto-revert-mode for the diary file
+(with-current-buffer (find-file-noselect diary-file)
+  (auto-revert-mode 1))
