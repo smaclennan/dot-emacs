24f1dae4caa835df1f9ad327e7b603787a65dd78
Sean MacLennan <seanm@seanm.ca>
2018-12-08 13:16:23 -0500

    More efficient version

--- rc/calendar-rc.el
+++ rc/calendar-rc.el
@@ -9,88 +9,70 @@
       calendar-latitude  '[45 23 north]
       calendar-longitude '[75 44 west])
 
-(my-feature-cond
- (holiday-christian-holidays
-    (setq holiday-christian-holidays nil
-	  holiday-hebrew-holidays nil
-	  holiday-islamic-holidays nil
-	  holiday-bahai-holidays nil
-	  holiday-oriental-holidays nil
-	  calendar-mark-holidays-flag t))
- (t
-  (setq christian-holidays nil
-	hebrew-holidays nil
-	islamic-holidays nil
-	bahai-holidays nil
-	oriental-holidays nil
-	mark-holidays-in-calendar t)))
+(if (boundp 'calendar-mark-holidays-flag)
+    (setq calendar-mark-holidays-flag t)
+  (setq mark-holidays-in-calendar t))
 
-(defun xmas-day (year)
-  "Given a year return the week day that Christmas is on. 1 = Monday."
-  (string-to-number
-   (format-time-string
-    "%u" (date-to-time
-	  (concat "Dec 25 00:00 " (number-to-string year))))))
-
-(defun ny-day (year)
-  "Given a year return the week day that New Years is on. 1 = Monday."
-  (string-to-number
-   (format-time-string
-    "%u" (date-to-time
-	  (concat "Jan 1 00:00 " (number-to-string year))))))
+(defun corrected-dow (date)
+  "Given a list of month, day, year return the week day. Sunday = 7."
+  (let ((dow (calendar-day-of-week date)))
+    (if (eq dow 0) 7 dow)))
 
 ;; Standard holidays too UScentric
 (setq calendar-holidays
-      '((holiday-fixed  2  2	"Groundhog Day")
-	(holiday-fixed  2 14	"Valentine's Day")
-	(holiday-float  2  1  3 "Family Day (S)")
-	(holiday-fixed  3 17	"St. Patrick's Day")
-	(holiday-fixed  4  1	"April Fools' Day")
-	(holiday-easter-etc -2	"Good Friday (S)")
-	(holiday-easter-etc 1	"Easter Monday")
-	(holiday-float  5  0  2	"Mother's Day")
-	(holiday-float  5  1 -1	"Victoria Day (S)" 24)
-	(holiday-float  6  0  3	"Father's Day")
-	(holiday-fixed  7  1	"Canada Day (S)")
-	(holiday-fixed  7 16	"Slackware 1.00 1993")
-	(holiday-float  8  1  1	"Civic Holiday")
-	(holiday-fixed  8 16	"Debian 1993")
-	(holiday-fixed  8 18	"Jeanine's Birthday")
-	(holiday-float  9  1  1	"Labour Day (S)")
-	(holiday-fixed  9 17	"Linux 0.01 1991")
-	(holiday-float 10  1  2	"Thanksgiving (S)")
-	(holiday-fixed 10 31	"Halloween")
-	(holiday-fixed 11  3	"Unix V1 1971")
-	(holiday-fixed 11 11	"Rememberance Day")
-	;; Christmas/Boxing Day/New Year's
-	(holiday-sexp '(if (> (xmas-day year) 5) (list 12 25 year))
-		      "Christmas")
-	(holiday-sexp '(let ((xmas-day (xmas-day year)))
-			 (if (eq xmas-day 7) ;; Sunday
-			     (list 12 26 year)
-			   (if (eq xmas-day 6) ;; Saturday
-			       (list 12 27 year)
-			     (list 12 25 year))))
-		      "Christmas (S)")
-	(holiday-sexp '(if (> (xmas-day year) 4) (list 12 26 year))
-		      "Boxing Day")
-	(holiday-sexp '(let ((xmas-day (xmas-day year)))
-			 (if (eq xmas-day 7) ;; Sunday
-			     (list 12 27 year)
-			   (if (>= xmas-day 5) ;; Saturday/Friday
-			     (list 12 28 year)
-			   (list 12 26 year))))
-		      "Boxing Day (S)")
-	(holiday-sexp '(if (> (xmas-day year) 4) (list 12 26 year))
-		      "New Year's Day")
-	(holiday-sexp '(let ((ny-day (ny-day year)))
-			 (if (eq ny-day 7) ;; Sunday
-			     (list 1 2 year)
-			   (if (eq ny-day 6) ;; Saturday
-			       (list 1 3 year)
-			     (list 1 1 year))))
-		      "New Year's Day (S)")
-	))
+      (append
+       '((holiday-fixed  2  2	"Groundhog Day")
+	 (holiday-fixed  2 14	"Valentine's Day")
+	 (holiday-float  2  1  3 "Family Day (S)")
+	 (holiday-fixed  3 17	"St. Patrick's Day")
+	 (holiday-fixed  4  1	"April Fools' Day")
+	 (holiday-easter-etc -2	"Good Friday (S)")
+	 (holiday-easter-etc 1	"Easter Monday")
+	 (holiday-float  5  0  2	"Mother's Day")
+	 (holiday-float  5  1 -1	"Victoria Day (S)" 24)
+	 (holiday-float  6  0  3	"Father's Day")
+	 (holiday-fixed  7  1	"Canada Day (S)")
+	 (holiday-fixed  7 16	"Slackware 1.00 1993")
+	 (holiday-float  8  1  1	"Civic Holiday")
+	 (holiday-fixed  8 16	"Debian 1993")
+	 (holiday-fixed  8 18	"Jeanine's Birthday")
+	 (holiday-float  9  1  1	"Labour Day (S)")
+	 (holiday-fixed  9 17	"Linux 0.01 1991")
+	 (holiday-float 10  1  2	"Thanksgiving (S)")
+	 (holiday-fixed 10 31	"Halloween")
+	 (holiday-fixed 11  3	"Unix V1 1971")
+	 (holiday-fixed 11 11	"Rememberance Day")
+	 ;; Christmas/Boxing Day/New Year's
+	 ;; 2009 - 2011 good years to check
+	 (holiday-sexp '(if (> (corrected-dow (list 12 25 year)) 5)
+			    (list 12 25 year)) "Christmas")
+	 (holiday-sexp '(if (memq (corrected-dow (list 12 26 year)) '(6 7 1))
+			    (list 12 26 year)) "Boxing Day")
+	 (holiday-sexp '(if (> (corrected-dow (list 1 1 year)) 5)
+			    (list 1 1 year))   "New Year's Day")
+	 (holiday-sexp '(let ((dow (corrected-dow (list 12 25 year))))
+			  (if (eq dow 7) ;; Sunday
+			      (list 12 26 year)
+			    (if (eq dow 6) ;; Saturday
+				(list 12 27 year)
+			      (list 12 25 year))))
+		       "Christmas (S)")
+	 (holiday-sexp '(let ((dow (corrected-dow (list 12 26 year))))
+			  (if (> dow 5) ;; Sunday/Saturday
+			      (list 12 28 year)
+			    (if (eq dow 1) ;; Monday
+				(list 12 27 year)
+			      (list 12 26 year))))
+		       "Boxing Day (S)")
+	 (holiday-sexp '(let ((dow (corrected-dow (list 1 1 year))))
+			  (if (eq dow 7) ;; Sunday
+			      (list 1 2 year)
+			    (if (eq dow 6) ;; Saturday
+				(list 1 3 year)
+			      (list 1 1 year))))
+		       "New Year's Day (S)"))
+       ;; Allow local holidays
+       holiday-local-holidays))
 
 ;; -------------------------
 ;; Diary is part of calendar
