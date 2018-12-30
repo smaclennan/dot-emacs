f8bd5682084ea31a72c13eb657cc9d38767d7855
Sean MacLennan <seanm@seanm.ca>
2018-12-30 13:43:00 -0500

    Don't ask about new tags files, just add them.

--- rc/etags-rc.el
+++ rc/etags-rc.el
@@ -2,4 +2,7 @@
   (xemacs
    (setq tags-build-completion-table nil ;; too slow
 	 tags-auto-read-changed-tag-files t))
-  (t (setq tags-revert-without-query t)))
+  (t
+   (setq tags-revert-without-query t)
+   (setq tags-add-tables t) ;; Don't ask, just do
+   ))
