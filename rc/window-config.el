871258b767350d4ab3be93f11f2e173936c0139b
Sean MacLennan <seanm@seanm.ca>
2019-01-04 22:27:51 -0500

    Merge window-config and laptop-mode

--- rc/window-config.el
+++ rc/window-config.el
@@ -3,76 +3,65 @@
 
 (defvar bells-and-whistles nil "*If non-nil, enable all graphical widgets.")
 
-(my-feature-cond
-  (xemacs
-   (setq frame-title-format
-	 '("XEmacs " emacs-program-version " " host-name ":"
-	   (buffer-file-name "%f" "%b")))
+(setq frame-title-format '("Emacs " emacs-version " " host-name ":"
+			   (buffer-file-name
+			    (:eval (abbreviate-file-name buffer-file-name))
+			    "%b")))
 
-   ;; Menubar
-   (setq menu-accelerator-enabled 'menu-fallback
-	 menu-accelerator-modifiers '(alt))
+(if (fboundp 'delete-selection-mode)
+    (delete-selection-mode)
+  (and (< emacs-major-version 23)
+       (would-like 'pc-select)
+       (pc-selection-mode)))
 
-   ;; Speedbar
-   (when (and bells-and-whistles (would-like 'speedbar))
-     (add-menu-button '("Tools")
-		      ["Speedbar" speedbar-frame-mode
-		       :style toggle
-		       :selected (and (boundp 'speedbar-frame)
-				      (frame-live-p speedbar-frame)
-				      (frame-visible-p speedbar-frame))]
-		      "--"))
+(unless bells-and-whistles
+  (setq use-dialog-box nil)
+  ;; Toolbar
+  (tool-bar-mode 0)
+  ;; Tooltips hang emacs over VPN
+  (tooltip-mode 0))
 
-   ;; Performance optimizations
-   ;; Use C-Insert and Shift-Insert for clipboard
-   (setq interprogram-cut-function nil
-	 interprogram-paste-function nil)
-
-   (setq shifted-motion-keys-select-region t)
-   (eval-when-compile (would-like 'pending-del))
-   (when (would-like 'pending-del)
-     (setq pending-delete-modeline-string "")
-     (turn-on-pending-delete))
-
-   (unless bells-and-whistles
-     (setq use-dialog-box nil)
-
-     ;; Gutter - turn it off
-     (if (boundp 'gutter-buffers-tab-enabled)
-	 (setq gutter-buffers-tab-enabled nil)
-       ;; Old way
-       (if (boundp 'default-gutter-visible-p)
-	   (set-specifier default-gutter-visible-p nil)))
-
-     ;; Toolbar
-     (set-specifier default-toolbar-visible-p nil))
-   ) ;; xemacs
-
-  (emacs
-   (setq frame-title-format '("Emacs " emacs-version " " host-name ":"
-			      (buffer-file-name
-			       (:eval (abbreviate-file-name buffer-file-name))
-			       "%b")))
-
-   (if (fboundp 'delete-selection-mode)
-       (delete-selection-mode)
-     (and (< emacs-major-version 23)
-	  (would-like 'pc-select)
-	  (pc-selection-mode)))
-
-   (unless bells-and-whistles
-     (setq use-dialog-box nil)
-     ;; Toolbar
-     (tool-bar-mode 0)
-     ;; Tooltips hang emacs over VPN
-     (tooltip-mode 0))
-
-   ;; Set the cursor properly for Emacs
-   (blink-cursor-mode 0)
-   (set-cursor-color "red")))
+;; Set the cursor properly for Emacs
+(blink-cursor-mode 0)
+(set-cursor-color "red")
 
 ;; Do this *after* setting the modeline colours
 (and nil (fboundp 'display-time) (display-time))
 
-;; Deal with laptop-mode after all fonts setup
-(load (concat rcfiles-directory "laptop-mode"))
+;; --------------------------------------------
+;; laptop mode
+
+(defvar laptop-mode nil
+  "*Set laptop mode (larger font).
+Setting laptop mode to 'auto tries to guess setting.")
+
+(defvar laptop-mode-font-size 120
+  "*The font size to use for laptop mode in pixels.
+When a list, it is the sizes for normal and laptop
+mode. Generally you don't need to setup the list, laptop-mode
+will do it for you.")
+
+(when (eq laptop-mode 'auto)
+  (if (eq window-system 'x)
+      (let ((out (shell-command-to-string "xrandr -q"))
+	    (count 0))
+	(while (string-match "^[^ ]+ connected" out)
+	  (setq count (1+ count))
+	  (setq out (replace-match "" nil nil out)))
+	(setq laptop-mode (eq count 1)))
+    (setq laptop-mode nil)))
+
+(unless (listp laptop-mode-font-size)
+  (setq laptop-mode-font-size (list (face-attribute 'default :height)
+				    laptop-mode-font-size)))
+
+(defun laptop-mode-toggle (&optional on)
+  "Toggle laptop-mode. Doesn't work that well on XEmacs :("
+  (interactive)
+  (setq laptop-mode (if on t (not laptop-mode)))
+  (set-face-attribute 'default nil :height
+		      (if laptop-mode
+			  (nth 1 laptop-mode-font-size)
+			(nth 0 laptop-mode-font-size))))
+
+(when laptop-mode (laptop-mode-toggle t))
