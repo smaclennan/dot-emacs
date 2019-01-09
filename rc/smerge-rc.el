5bd8e6f1b6090bb552c41a28a163cabacf5faee9
Sean MacLennan <smaclennan@diablo-technologies.com>
2019-01-08 22:24:26 -0500

    Make the smerge options match smerge-rc. Seems silly otherwise

--- rc/smerge-rc.el
+++
@@ -1,6 +0,0 @@
-(setq smerge-diff-excludes
-      '("*.o" "*.obj" "*.a" "*.lib" "*~" ".#*" "CVS" ".svn"
-	".git" "*.cmd" "*.lo" "*.ko" ".tmp_versions" "*.Plo"
-	"modules.order" "*.elc" "*.mod.c" "TAGS" "*.builtin"))
-
-(setq smerge-diff-options "-w")
