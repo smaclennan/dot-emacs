e525942454ac66f95779e9c2232da89017a88e67
Sean MacLennan <seanm@seanm.ca>
2019-01-13 13:11:03 -0500

    If we don't have a diary file; don't setup for diary

--- rc/appt-rc.el
+++ rc/appt-rc.el
@@ -1,15 +1,16 @@
 ;; Use (diary) to display it
 (setq appt-display-diary nil)
 
-;; We need the full path for matching
-(setq diary-file (expand-file-name diary-file))
+(when (file-readable-p diary-file)
+  ;; We need the full path for matching
+  (setq diary-file (expand-file-name diary-file))
 
-;; Update appt if the diary file changed
-(defun check-if-diary-changed ()
-  (when (equal (buffer-file-name) diary-file)
-    (appt-check t)))
-(add-hook 'after-revert-hook 'check-if-diary-changed)
+  ;; Update appt if the diary file changed
+  (defun check-if-diary-changed ()
+    (when (equal (buffer-file-name) diary-file)
+      (appt-check t)))
+  (add-hook 'after-revert-hook 'check-if-diary-changed)
 
-;; Enable auto-revert-mode for the diary file
-(with-current-buffer (find-file-noselect diary-file)
-  (auto-revert-mode 1))
+  ;; Enable auto-revert-mode for the diary file
+  (with-current-buffer (find-file-noselect diary-file)
+    (auto-revert-mode 1)))
