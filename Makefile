6e38b531d75ed428382037ddc7adc1a3717f10fa
Sean MacLennan <seanm@seanm.ca>
2019-02-01 19:36:47 -0500

    Updating the loaddef file on demand broke make -j. Go back to building the loaddef file in it's own rule.

--- Makefile
+++ Makefile
@@ -1,14 +1,10 @@
 # I have had problems byte-compiling the rc directory
 # Emacs will load the rc files multiple times
 
-all:	TAGS
+all:
 	@$(MAKE) -C lisp PDIR=lisp/ -s $(MFLAGS) all
 	@$(MAKE) -C misc PDIR=misc/ -s $(MFLAGS) all
-#	@$(MAKE) -C rc   PDIR=rc    -s $(MFLAGS) all
-
-TAGS:	*.el
-	@echo Building tags...
-	@etags *.el lisp/*.el misc/*.el
+#	@$(MAKE) -C rc   PDIR=rc/   -s $(MFLAGS) all
 
 clean:
 	find . -name "*.elc" -delete
--- lisp/batch-helper.el
+++ lisp/batch-helper.el
@@ -4,22 +4,12 @@
   (add-to-list 'load-path (concat user-emacs-directory dir))
   (load (concat dir "-loaddefs") t t))
 
-(defun basename (name)
-  (if (string-match "/\\([^/]+\\)/?$" name)
-      (match-string 1 name)
-    name))
+(eval-when-compile (require 'autoload))
 
-(unless (equal user-emacs-directory default-directory)
-  ;; This expects one .el and updates the autoload file
-  (let (file)
-    (dolist (one command-line-args-left)
-      (when (string-match "^[a-zA-Z0-9._-]+\\.el$" one)
-	(when file (message "Warning: more than one .el file"))
-	(setq file one)))
-
-    (if file
-	;; Emacs 23.1 does not support outfile arg to `update-file-autoloads'
-	(let ((generated-autoload-file
-	       (concat default-directory (basename default-directory) "-loaddefs.el")))
-	  (update-file-autoloads file t))
-      (message "Warning: No lisp file found"))))
+(defun update-loadfile ()
+  "This is meant to be called in batch mode. You must specify the loadfile."
+  (unless (eq system-type 'windows-nt)
+    (setq autoload-excludes (list (expand-file-name "windows-nt.el"))))
+  (let ((generated-autoload-file
+	 (expand-file-name (car command-line-args-left))))
+    (update-directory-autoloads default-directory)))
