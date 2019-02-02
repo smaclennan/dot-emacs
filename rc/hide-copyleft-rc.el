5bae1c94186c3d7a53a5f5c04ae34d4227d5c3e9
Sean MacLennan <seanm@seanm.ca>
2019-02-01 20:20:46 -0500

    Add QNX copyright to hide-copylefts

--- rc/hide-copyleft-rc.el
+++ rc/hide-copyleft-rc.el
@@ -2,6 +2,6 @@
 ;; If you're sure you're not gonna get sued, you can do something like this
 ;; in your .emacs file.
 
-(add-to-list
- 'copylefts-to-hide
- '(" \\* The Apache Software License, Version 1\\.1" . " \\*/"))
+(append copylefts-to-hide
+	'((" \\* The Apache Software License, Version 1\\.1" . " \\*/")
+	  (" \\* \\$QNXLicenseC:" . " \\* \\$")))
