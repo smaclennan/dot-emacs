6f25d9ddbbf648155ba8c223762fa762e7b5a92f
Sean MacLennan <seanm@seanm.ca>
2019-02-06 20:16:32 -0500

    Make sure both QNX_BASE_DIR and QNX_SANDBOX set.

--- work.el
+++ work.el
@@ -1,3 +1,6 @@
-(let ((qnx-base (getenv "QNX_BASE_DIR")))
-  (when qnx-base
-    (load (concat (file-name-as-directory qnx-base) "emacs/qnx") t)))
+(let ((qnx-base (getenv "QNX_BASE_DIR"))
+      (qnx-sandbox (getenv "QNX_SANDBOX")))
+  (and qnx-base qnx-sandbox
+       (setq qnx-base (file-name-as-directory qnx-base))
+       (setq qnx-sandbox (file-name-as-directory qnx-sandbox))
+       (load (concat qnx-base "emacs/qnx") t)))
