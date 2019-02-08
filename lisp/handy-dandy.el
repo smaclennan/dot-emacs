1fce9d2c630d703fb659dbba6168945a1ee275a7
Sean MacLennan <seanm@seanm.ca>
2019-02-07 20:07:06 -0500

    dup-line can now comment out the old line. This was the old behavior and I miss it. Removed friendly-time since I no longer need it.

--- lisp/handy-dandy.el
+++ lisp/handy-dandy.el
@@ -92,77 +92,15 @@ If nil, defaults to \"`user-full-name' <`user-mail-address'>\".")
 
 ;;;###autoload
 (defun dup-line (&optional arg)
-  "Duplicate the current line.
-A negative arg comments out the `new' line[s]."
-  (interactive "*p")
-  (let ((line (buffer-substring
-	       (progn (end-of-line) (point))
-	       (progn (beginning-of-line) (point)))))
+  "Duplicate the current line leave the point at the start of the new line.
+An ARG comments out the old line."
+  (interactive "P")
+  (let ((line (buffer-substring (line-end-position)
+				(progn (beginning-of-line) (point)))))
     ;; The above leaves the point at the start of the current line
-    (dotimes (iter arg) (insert line) (newline))))
-
-;; This is a silly little Lisp function for people who don't care
-;; exactly what time it is.
-
-(defvar ft-time-strings
-  '(("o'clock" t)
-    ("quarter past")
-    ("half past")
-    ("quarter to"))) ;; quarter of
-
-(defvar ft-hour-strings
-  '("twelve"
-    "one"
-    "two"
-    "three"
-    "four"
-    "five"
-    "six"
-    "seven"
-    "eight"
-    "nine"
-    "ten"
-    "eleven"))
-
-(defvar ft-short-version nil
-  "*If non-nil display short version of friendly time.
- e.g. 5:30 rather than half past five")
-
-(defun ft-get-time (&optional time)
-  "Calculate current hour and quarter of an hour. If `TIME' is nil,
-uses current time. Returns (HOUR QUARTER)."
-  (let* ((now (decode-time time))
-	 (seconds (+ (nth 0 now) (* (nth 1 now) 60)))
-	 (hour (nth 2 now))
-	 quarter)
-    (cond
-     ((< seconds  450) (setq quarter 0))
-     ((< seconds 1350) (setq quarter 1))
-     ((< seconds 2250) (setq quarter 2))
-     ((< seconds 3150) (setq quarter 3 hour (1+ hour)))
-     (t (setq quarter 0 hour (1+ hour))))
-    (if (>= hour 12) (setq hour (- hour 12)))
-    (list hour quarter)))
-
-;;;###autoload
-(defun friendly-time (&optional show time short)
-  "Display a friendly version of the time.
-Shows the time to the nearest quarter of an hour."
-  (interactive "p")
-  (let* ((now (ft-get-time time))
-	 (hour (nth 0 now))
-	 (quarter (nth 1 now))
-	 str)
-    (if (or ft-short-version short)
-	(setq str (format "%d:%02d" hour (* quarter 15)))
-      (let ((ts (nth quarter ft-time-strings)))
-	(if (nth 1 ts)
-	    (setq str (concat (nth hour ft-hour-strings) " "
-			      (nth 0 ts)))
-	  (setq str (concat (nth 0 ts) " "
-			    (nth hour ft-hour-strings))))))
-    (if show (message "%s" str))
-    str))
+    (if (and arg comment-start)
+	(insert comment-start line comment-end "\n")
+      (insert line "\n"))))
 
 (defun mine (&optional all)
   "Return a list of all my .el files. If `all' is non-nil,
