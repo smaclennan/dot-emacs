53aa8576bfdc0956197e579cb55f538968ea1bc5
Sean MacLennan <seanm@seanm.ca>
2019-02-24 14:04:32 -0500

    Don't support really old kernels

--- sys/gnu-linux.el
+++ sys/gnu-linux.el
@@ -1,17 +1,2 @@
-
-(defvar sys-cpu-dir "/sys/bus/cpu/devices"
-  "The /sys directory containing cpu* files.")
-
-;; Used by `cpuinfo-num-processors'
 (defun sys-nproc ()
-  (if (file-exists-p sys-cpu-dir)
-      ;; Does this even not exist any more?
-      (length (directory-files sys-cpu-dir nil "cpu[0-9]*"))
-    (let ((procs))
-      (with-temp-buffer
-	;; insert-file-contents does not work on /proc
-	(call-process "cat" nil t nil "/proc/cpuinfo")
-	(goto-char (point-min))
-	(while (re-search-forward "^processor" nil t)
-	  (setq procs (1+ procs))))
-      procs)))
+  (length (directory-files "/sys/bus/cpu/devices" nil "cpu[0-9]*")))
