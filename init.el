0e0faa8cdd142da13193435df3eed06cd945d230
Sean MacLennan <seanm@seanm.ca>
2019-03-08 20:18:07 -0500

    More cleanup

--- init.el
+++ init.el
@@ -1,7 +1,4 @@
-;; Emacs setup -*- Mode:emacs-lisp -*-
-;; This file should work with Emacs >= 24
-
-;;{{{ Configuration variables / functions
+;; Emacs initialization. Should work with Emacs >= 24.
 
 ; This is the one key binding I must have... switch ASAP
 (global-set-key "\C-x\C-b" 'switch-to-buffer)
@@ -11,8 +8,7 @@
   (load (concat dir "-loaddefs") t t))
 
 ;; This is when Emacs went insane about common lisp
-(when (< emacs-major-version 25)
-  (load (concat user-emacs-directory "compat")))
+(if (< emacs-major-version 25) (load (concat user-emacs-directory "compat")))
 
 ;; The user-init file allows for user/machine specific
 ;; initialization. It must be very early for variables like
@@ -22,9 +18,9 @@
 
 (rcfiles-register-rc-files)
 
-;;}}}
-
-;;{{{ Basic Customization
+(if window-system
+    (load (concat rcfiles-directory "/window-config"))
+  (menu-bar-mode -1)) ;; Yes, Emacs has a menu bar in console mode
 
 (setq track-eol t
       kill-whole-line t
@@ -42,55 +38,41 @@
 ;; Let's try making _ part of a "word". C & C++ done in cc-mode-rc.el
 (modify-syntax-entry ?_ "w" (standard-syntax-table))
 
-;; Windowing System Customization
-(if window-system
-    (load (concat rcfiles-directory "/window-config"))
-  ;; Yes, Emacs has a menu bar in console mode
-  (if (fboundp 'menu-bar-mode)
-      (menu-bar-mode -1)))
-
-;;}}}
-
-;;{{{ Keys
+;;; ------------------------------------------------------------
+;;; Keys
 
 ;;;; Function keys.
-(global-set-key [XF86_Switch_VT_1] (global-key-binding [f1]))
+;; Shifted function keys: [(shift f1)] == [XF86_Switch_VT_1]
 (global-set-key [f1]            'find-file)
 (global-set-key [f2]		'undo)
 (global-set-key [f3]		'isearch-repeat-forward)
 (global-set-key [(shift f3)]    'isearch-repeat-backward)
-(global-set-key [XF86_Switch_VT_3] 'isearch-repeat-backward)
 (global-set-key [f4]		'next-error)
 (global-set-key [f5]		'query-replace)
 (global-set-key [(shift f5)]    'query-replace-regexp)
-(global-set-key [XF86_Switch_VT_5] 'query-replace-regexp)
 (global-set-key [f6]		'git-grep-at-point)
 (global-set-key [(shift f6)]	'git-grep)
 (global-set-key [(control f6)]	'git-grep-toggle-top-of-tree)
-(global-set-key [XF86_Switch_VT_6] 'git-grep)
 (global-set-key [f7]		'compile)
 (global-set-key [(shift f7)]    'my-make-clean)
-(global-set-key [XF86_Switch_VT_7] 'my-make-clean)
 (global-set-key [(control f7)]	'my-set-compile)
 (global-set-key [f8] 'my-grep)
 (global-set-key [(shift f8)] 'my-grep-find)
 (global-set-key [(control f8)]	'my-checkpatch)
 (global-set-key [f9]		'my-isearch-word-forward)
 (global-set-key [(shift f9)]    'my-toggle-case-search)
-(global-set-key [XF86_Switch_VT_9] 'my-toggle-case-search)
 (global-set-key [f10]		'xref-find-definitions)
-(global-set-key "\M-."		'xref-find-definitions-prompt)
 (global-set-key [(shift f10)]   'pop-tag-mark)
-(global-set-key [XF86_Switch_VT_10] 'pop-tag-mark)
-(global-set-key [f20] 'pop-tag-mark)
 (global-set-key [(control f10)] 'xref-find-references)
 (global-set-key [f11] nil) ;; I keep f11 free for temporary bindings
 (global-set-key [(shift f11)] 'my-show-messages)
-(global-set-key [XF86_Switch_VT_11] 'my-show-messages)
 (global-set-key [f12]		'revert-buffer)
 (global-set-key [(shift f12)]	'lxr-next-defined)
-(global-set-key [XF86_Switch_VT_12] 'lxr-next-defined)
 (global-set-key [(control f12)] 'lxr-defined-at-point)
+
+(global-set-key "\M-."		'xref-find-definitions-prompt)
+(global-set-key [(meta right)] 'forward-sexp)
+(global-set-key [(meta left)]  'backward-sexp)
 
 (global-set-key "\C-cd" 'dup-line)
 (global-set-key "\C-ce" 'errno-string)
@@ -101,13 +83,10 @@
 (global-set-key "\C-c8" '80-scan)
 (global-set-key "\C-c9" '80-cleanup) ;; shift-8 and ctrl-8 did not work
 
-(global-set-key [(meta right)] 'forward-sexp)
-(global-set-key [(meta left)]  'backward-sexp)
-
 ;; For some reason this doesn't have a key binding
 (global-set-key "\C-hz" 'apropos-variable)
 
-;;; --- GNU Emacs really really needs a signal-error-on-buffer-boundary
+;;; ---- GNU Emacs really really needs a signal-error-on-buffer-boundary
 
 ;; Using defadvice for these functions breaks minibuffer history
 (defun my-previous-line (arg)
@@ -128,6 +107,8 @@
 
 (global-set-key (kbd "<up>") 'my-previous-line)
 (global-set-key (kbd "<down>") 'my-next-line)
+(global-set-key "\C-p" 'my-previous-line)
+(global-set-key "\C-n" 'my-next-line)
 
 (defadvice scroll-down (around my-scroll-down activate)
   "`scroll-down' with no signal on end-of-buffer."
@@ -174,11 +155,9 @@ the identifier."
 (global-set-key "\M-#"		'my-calc)
 (global-set-key [(iso-left-tab)] 'tab-to-tab-stop)
 
-;;}}}
+;;; ------------------------------------------------------------
+;;; Packages
 
-;;{{{ Packages
-
-;;; -------------------------------------------------------------------------
 (defvar commit-names '("COMMIT_EDITMSG" "svn-commit.tmp" "README")
   "* List of commit buffer names.")
 
@@ -191,18 +170,12 @@ the identifier."
 	  (text-mode))))))
 (add-hook 'find-file-hooks 'check-for-commit t)
 
-;;; -------------------------------------------------------------------------
-;;; Some edit-utils packages
-;; (icomplete-mode 1)
-;; (ido-mode 1))
 (iswitchb-mode 1)
-
 (show-paren-mode t)
 (delete-selection-mode)
 
 (global-set-key "\C-x\C-b" (global-key-binding "\C-xb"))
 
-;;; -------------------------------------------------------------------------
 ;; The auto-save.el and backup.el packages collect files in one place
 ;; I added the following to my crontab:
 ;; 13 5 * * * find $HOME/.backup -mtime +7 -delete
@@ -212,22 +185,18 @@ the identifier."
 (setq auto-save-file-name-transforms `((".*" "~/.autosave/" t)))
 (setq backup-directory-alist '((".*" . "~/.backup")))
 
-;;; ------------------------------------------------------------
-;; Start the server program
 (unless noninteractive (server-start))
 
-;;}}}
-
-;;{{{ Optional Init files
+;;; ------------------------------------------------------------
+;;; Optional Init files
 
 ;; Load a file called sys/`system-type' if it exists.
 (load (concat user-emacs-directory "sys/" (symbol-name system-type)) t)
 
 (load (concat user-emacs-directory "work") t)
 
-;;}}}
-
-;;{{{ Final results
+;;; ------------------------------------------------------------
+;;; Final results
 
 (defun friendly-message ()
   (interactive)
@@ -244,7 +213,5 @@ the identifier."
   ;; stupid^h^h^h^h^h "useful" message that overwrites my nice friendly
   ;; one. So use a timer to get past them.
   (run-at-time .25 nil 'friendly-message))
-
-;;}}}
 
 ;; end of .emacs "May the `(' be with `)'"
