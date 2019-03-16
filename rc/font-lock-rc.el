a47376f1310e0034d318c70b214f74c322c1756e
Sean MacLennan <seanm@seanm.ca>
2019-03-16 13:05:14 -0400

    I haven't used, nor seen, a greyscale monitor since the mid-90s

--- rc/font-lock-rc.el
+++ rc/font-lock-rc.el
@@ -5,16 +5,23 @@
 	      font-lock-verbose nil
 	      font-lock-maximum-size nil)
 
+;; Change some default  faces
+(make-face-bold 'font-lock-function-name-face)
+
+(if window-system
+    (set-face-foreground 'font-lock-comment-face "FireBrick")
+  ;; Consoles have less colors to play with
+  (set-face-foreground 'font-lock-comment-face "red")
+  (set-face-foreground 'font-lock-string-face "green")
+  (set-face-foreground 'font-lock-keyword-face "blue")
+  (set-face-foreground 'font-lock-variable-name-face "purple")
+  )
+
 ;; -------------------------------------------------------------------------
 ;; font-lock-comment-warn
 ;; I want all comments with my initials (SAM) at the start to be very bold
 (defface font-lock-comment-warn-face
-  '((((class color))  (:foreground "red" :bold t :italic t))
-    (((class grayscale) (background light))
-     (:foreground "DimGray" :bold t :italic t))
-    (((class grayscale) (background dark))
-     (:foreground "LightGray" :bold t :italic t))
-    (t (:bold t)))
+  '((default (:foreground "red" :bold t :italic t)))
   "Font Lock mode face used to highlight warning comments."
   :group 'font-lock-faces)
 
@@ -26,15 +33,3 @@
   (unless re (setq re sh-comment-warn))
   (let ((keyword (list (list re 0 (quote `font-lock-comment-warn-face) t))))
     (font-lock-add-keywords mode keyword)))
-
-;; Change a couple of faces
-(make-face-bold 'font-lock-function-name-face)
-
-(if window-system
-    (set-face-foreground 'font-lock-comment-face "FireBrick")
-  ;; Consoles have less colors to play with
-  (set-face-foreground 'font-lock-comment-face "red")
-  (set-face-foreground 'font-lock-string-face "green")
-  (set-face-foreground 'font-lock-keyword-face "blue")
-  (set-face-foreground 'font-lock-variable-name-face "purple")
-  )
