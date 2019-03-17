a730327cf84603de6961121a57c718e360793e17
Sean MacLennan <seanm@seanm.ca>
2019-03-17 19:50:08 -0400

    Make the my-grep and my-grep-find use the same basic code

--- lisp/my-grep.el
+++ lisp/my-grep.el
@@ -7,14 +7,24 @@
 (defvar my-grep-files-history nil
   "The minibuffer history list for `my-grep' and `my-grep-find's FILES argument.")
 
-(defun my-grep-exts ()
-  (let ((ext (if buffer-file-name (file-name-extension buffer-file-name))))
-    (if ext
-	;; This is my preference... and it is my code.
-	(if (or (string= ext "c") (string= ext "h"))
-	    "*.[ch]"
-	  (concat "*." ext))
-      "*")))
+(defun my-grep-interactive ()
+  (let* ((word (current-word))
+	 (ext (if buffer-file-name (file-name-extension buffer-file-name)))
+	 (exts (if ext
+		   ;; This is my preference... and it is my code.
+		   (if (or (string= ext "c") (string= ext "h"))
+		       "*.[ch]"
+		     (concat "*." ext))
+		 "*"))
+	  (rprompt (concat "Regex [" word "]: "))
+	  (fprompt (concat "Files [" exts "]: ")))
+     (list (read-string rprompt nil 'my-grep-regex-history word)
+	   (read-string fprompt nil 'my-grep-files-history exts))))
+
+(defun my-grep-doit(cmd)
+  (when current-prefix-arg
+    (setq cmd (read-string "Cmd: " cmd 'grep-history)))
+  (grep cmd))
 
 ;;;###autoload
 (defun my-grep (regex files)
@@ -24,21 +34,9 @@ current extension.
 
 With a prefix arg you can edit the grep command before the grep
 is run. This allows extra flags like -i etc. Don't remove the -nH or
-`next-error' will not work.
-
-Non-interactively this is just:
-(grep (concat my-grep-prog \" \\\"\" regex \"\\\" \" files))"
-  (interactive
-   (let* ((word (current-word))
-	  (exts (my-grep-exts))
-	  (rprompt (concat "Regex [" word "]: "))
-	  (fprompt (concat "Files [" exts "]: ")))
-     (list (read-string rprompt nil 'my-grep-regex-history word)
-	   (read-string fprompt nil 'my-grep-files-history exts))))
-  (let ((cmd (concat my-grep-prog " \"" regex "\" " files)))
-    (when current-prefix-arg
-      (setq cmd (read-string "Cmd: " cmd 'grep-history)))
-    (grep cmd)))
+`next-error' will not work."
+  (interactive (my-grep-interactive))
+  (my-grep-doit (concat my-grep-prog " \"" regex "\" " files)))
 
 ;;;###autoload
 (defun my-grep-find (regex files)
@@ -49,19 +47,7 @@ find all files.
 
 With a prefix arg you can edit the grep command before the grep
 is run. This allows extra flags like -i etc. It is generally a
-VBI (Very Bad Idea) to remove any of the existing flags.
-
-Non-interactively this is just:
-(grep (concat \"find -name '\" files \"' -print0 |
-              xargs -0 \" my-grep-prog \" '\" regex \"'\"))"
-  (interactive
-   (let* ((word (current-word))
-	  (exts (my-grep-exts))
-	  (rprompt (concat "Regex [" word "]: "))
-	  (fprompt (concat "Files [" exts "]: ")))
-     (list (read-string rprompt nil 'my-grep-regex-history word)
-	   (read-string fprompt nil 'my-grep-files-history exts))))
-  (let ((cmd (concat "find -name '" files "' -print0 | xargs -0 " my-grep-prog " '" regex "'")))
-    (when current-prefix-arg
-      (setq cmd (read-string "Cmd: " cmd 'grep-history)))
-    (grep cmd)))
+VBI (Very Bad Idea) to remove any of the existing flags."
+  (interactive (my-grep-interactive))
+  (my-grep-doit (concat "find -name '" files "' -print0|"
+			"xargs -0 " my-grep-prog " '" regex "'")))
