10001299b67105da8eeb2d3f1627d380e64aa1a4
Sean MacLennan <seanm@seanm.ca>
2019-03-24 14:44:22 -0400

    Make it easier to enable work later.

--- work.el
+++ work.el
@@ -1,6 +1,7 @@
-(condition-case nil
-    (and
-     (setq qnx-base (file-name-as-directory (getenv "QNX_BASE_DIR")))
-     (setq qnx-sandbox (file-name-as-directory (getenv "QNX_SANDBOX")))
-     (load (concat qnx-base "emacs/qnx") t))
-  (error nil))
+(defun qnx-load-qnx ()
+  (interactive)
+  (let ((qnx-base (file-name-as-directory (getenv "QNX_BASE_DIR"))))
+    (and qnx-base (getenv "QNX_SANDBOX")
+	 (load (concat qnx-base "emacs/qnx") t))))
+
+(condition-case nil (qnx-load-qnx) (error nil))
