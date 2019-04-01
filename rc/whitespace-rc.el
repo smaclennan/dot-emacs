176da737d82249c1f6041ed99f28a74d1f33fd20
Sean MacLennan <seanm@seanm.ca>
2019-04-01 18:32:08 -0400

    whitespace mode

---
+++ rc/whitespace-rc.el
@@ -0,0 +1,13 @@
+(defun whitespace-long-lines (on)
+  "Disable displaying long lines in whitespace mode.
+With a prefix arg, enable it."
+  (interactive "P")
+  (if on
+      (add-to-list 'whitespace-style 'lines)
+    (setq whitespace-style (delete 'lines whitespace-style)))
+  (when (my-interactive-p)
+    ;; We need to toggle mode to get new setting to take.
+    (whitespace-mode 0)
+    (whitespace-mode 1)))
+
+(whitespace-long-lines nil)
