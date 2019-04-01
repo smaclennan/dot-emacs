14f2a70706ddb40b72818b86cc2e68e42f7741d0
Sean MacLennan <seanm@seanm.ca>
2019-04-01 19:44:13 -0400

    With whitespace-mode we don't need eighty. Let's try lines-tail to better match eighty.

--- rc/whitespace-rc.el
+++ rc/whitespace-rc.el
@@ -3,8 +3,9 @@
 With a prefix arg, enable it."
   (interactive "P")
   (if on
-      (add-to-list 'whitespace-style 'lines)
-    (setq whitespace-style (delete 'lines whitespace-style)))
+      (add-to-list 'whitespace-style 'lines-tail)
+    (setq whitespace-style (delete 'lines whitespace-style))
+    (setq whitespace-style (delete 'lines-tail whitespace-style)))
   (when (my-interactive-p)
     ;; We need to toggle mode to get new setting to take.
     (whitespace-mode 0)
