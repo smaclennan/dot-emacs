9184cbe4cd67c719ed28c9bb1d4d9fc8578043e6
Sean MacLennan <seanm@seanm.ca>
2019-04-17 19:18:14 -0400

    Need weather on non-windows systems too

--- lisp/weather.el
+++ lisp/weather.el
@@ -1,6 +1,10 @@
 (defvar weather-url "https://weather.gc.ca/city/pages/on-118_metric_e.html"
   "The url for the weather. Must be weather.gc.ca.")
 
+(defvar weather-fancy t
+  "Display a fancy version of the weather in a buffer.
+Only works for X11.")
+
 ;;;###autoload
 (defun weather ()
   (interactive)
@@ -15,20 +19,22 @@
       (re-search-forward "class=\"wxo-imperial-hide wxo-city-hidden\">\\([0-9]+\\)")
       (setq tempf (match-string 1)))
 
-    ;; get the gif if necessary
-    (setq giffile (concat "~/." gif))
-    (unless (file-exists-p giffile)
-      (let ((gifurl (concat "https://weather.gc.ca/" gif)))
-	(with-current-buffer (url-retrieve-synchronously gifurl t)
-	  (goto-char (point-min))
-	  (let ((case-fold-search nil)) (search-forward "GIF"))
-	  (delete-region (point-min) (match-beginning 0))
-	  (write-file giffile))))
+    (if (and weather-fancy (eq window-system 'x))
+	(progn
+	  ;; get the gif if necessary
+	  (setq giffile (concat "~/." gif))
+	  (unless (file-exists-p giffile)
+	    (let ((gifurl (concat "https://weather.gc.ca/" gif)))
+	      (with-current-buffer (url-retrieve-synchronously gifurl t)
+		(goto-char (point-min))
+		(let ((case-fold-search nil)) (search-forward "GIF"))
+		(delete-region (point-min) (match-beginning 0))
+		(write-file giffile))))
 
-    (with-current-buffer (get-buffer-create "*weather*")
-      (erase-buffer)
-      (insert-image (create-image giffile))
-      (insert " " conditions " " tempc "\u2103 " tempf "\u2109\n"))
+	  (with-current-buffer (get-buffer-create "*weather*")
+	    (erase-buffer)
+	    (insert-image (create-image giffile))
+	    (insert " " conditions " " tempc "\u2103 " tempf "\u2109\n"))
 
-    (display-buffer-at-bottom (get-buffer "*weather*") '((window-height . 6)))
-    ))
+	  (display-buffer-at-bottom (get-buffer "*weather*") '((window-height . 6))))
+	(message "%s  %sC  %sF" conditions tempc tempf))))
