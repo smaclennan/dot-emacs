ee24d7997fd3a65004466e027306756af8e1172f
Sean MacLennan <smaclennan@diablo-technologies.com>
2019-05-08 18:01:56 -0400

    With doorknob, I don't need this

--- rc/time-rc.el
+++
@@ -1,8 +0,0 @@
-;; display-time
-
-;; want angry mail
-(setq display-time-use-mail-icon t)
-(defface angry-mail-face '((t (:background "red")))
-  "Make the mail face angry."
-  :group 'display-time)
-(setq display-time-mail-face 'angry-mail-face)
