3481513c4e4fff87f8f67cd4ae3f4b0acc176d52
Sean MacLennan <seanm@seanm.ca>
2019-05-09 22:09:25 -0400

    batch-helper needs to load sys-loaddefs

--- lisp/batch-helper.el
+++ lisp/batch-helper.el
@@ -1,6 +1,6 @@
 ;; This is meant to be used to compile lisp files in batch mode.
 
-(dolist (dir '("lisp" "misc"))
+(dolist (dir '("lisp" "misc" "sys"))
   (add-to-list 'load-path (concat user-emacs-directory dir))
   (load (concat dir "-loaddefs") t t))
 
