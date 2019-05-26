1bcbad59ca97a7fdbb41c1c8ea9f3c69723cf8de
Sean MacLennan <seanm@seanm.ca>
2019-05-26 17:35:24 -0400

    A couple of BSD specific initializations

--- sys/berkeley-unix.el
+++ sys/berkeley-unix.el
@@ -1,3 +1,9 @@
+(setq compile-command "gmake ")
+(setq make-clean-command "gmake clean all")
+
+(setq local-compile-cc "cc -O2 -Wall")
+(setq local-compile-c++ "c++ -O2 -Wall")
+
 (defvar page-size nil "Page size filled in by `sys-mem'.")
 
 (defun sysctl (arg)
