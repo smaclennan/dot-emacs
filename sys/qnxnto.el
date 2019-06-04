a5d0704ac9a1445dec1190627244d8111cc73724
Sean MacLennan <seanm@seanm.ca>
2019-06-04 18:40:43 -0400

    Faster sys-mem for QNX

--- sys/qnxnto.el
+++ sys/qnxnto.el
@@ -5,12 +5,20 @@
     (call-process "pidin" nil t nil "info")
     (count-matches "^Processor" (point-min))))
 
+(defvar sys-total-mem nil
+  "Total system memory. Filled in by `sys-mem'.")
+
 ;;;###autoload
 (defun sys-mem ()
-  "Return the total and free memory reported by pidin."
-  (with-temp-buffer
-    (call-process "pidin" nil t nil "info")
-    (goto-char (point-min))
-    (re-search-forward "Freemem:\\([0-9]+\\)MB/\\([0-9]+\\)MB")
-    (list (* (string-to-number (match-string 2)) #x100000)
-	  (* (string-to-number (match-string 1)) #x100000))))
+  "Return the total and free memory."
+  (unless sys-total-mem
+    ;; First time we need to call pidin to get total mem
+    (with-temp-buffer
+      (call-process "pidin" nil t nil "info")
+      (goto-char (point-min))
+      (re-search-forward "Freemem:[0-9]+MB/\\([0-9]+\\)MB")
+      (setq sys-total-mem
+	    (* (string-to-number (match-string 1)) #x100000))))
+  (list sys-total-mem
+	;; Yes ls -ld /proc = free memory
+	(nth 7 (file-attributes "/proc"))))
