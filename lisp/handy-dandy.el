27d12a64e3131a4fcf39edc913764514066ac004
Sean MacLennan <seanm@seanm.ca>
2019-06-20 21:14:10 -0400

    And.... we are moving them to handy-dandy

--- lisp/handy-dandy.el
+++ lisp/handy-dandy.el
@@ -180,3 +180,48 @@ underscore."
     (if (string-match "[a-zA-Z0-9]" (string ch))
 	(insert ch two one)
       (insert two ch one))))
+
+;;;###autoload
+(defun cpuinfo ()
+  "Describes the current cpus."
+  (interactive)
+  (let* ((info (sys-cpuinfo))
+	 (vendor (cadr info)))
+    ;; Pretty print common vendor ids
+    (cond
+     ((string= "GenuineIntel" vendor) (setq vendor "Intel"))
+     ((string-match "Authentic ?AMD" vendor) (setq vendor "AMD"))
+     ((string= "CentaurHauls" vendor) (setq vendor "VIA")))
+
+    (message "%s Vendor %s Family %d Model %d Step %d Procs %d"
+	     (nth 0 info) vendor (nth 2 info) (nth 3 info)
+	     (nth 4 info) (sys-nproc))))
+
+;;;###autoload
+(defun cpuinfo-has-flag (flag &optional show)
+  "Does the cpu have FLAG defined."
+  (interactive "sFlag: \np")
+  (let ((has-flag (member flag (sys-cpu-flags))))
+    (when show
+      (message "%s %s" flag (if has-flag "yes" "no")))
+    has-flag))
+
+;; Not really cpuinfo... but makes sense here
+(defun mem-human-readable (mem)
+  (cond
+   ((> mem 1073741824)
+    (format "%.1fG" (/ mem 1073741824.0)))
+   ((> mem 1048576)
+    (format "%.1fM" (/ mem 1048576.0)))
+   ((> mem 1024)
+    (format "%.1fK" (/ mem 1024.0)))
+   (t (format "%d" mem))))
+
+;;;###autoload
+(defun memory ()
+  "Display total and free memory."
+  (interactive)
+  (let ((mem (sys-mem)))
+    (message "total %s  free %s"
+	     (mem-human-readable (car mem))
+	     (mem-human-readable (cadr mem)))))
