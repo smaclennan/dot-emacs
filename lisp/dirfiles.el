1ed8ab48fbe6ac0694a4b8eeddfead3089a7f83a
Sean MacLennan <seanm@seanm.ca>
2019-06-28 19:42:27 -0400

    directory-files-and-attributes slightly faster than directory-files and then file-directory-p.

--- lisp/dirfiles.el
+++ lisp/dirfiles.el
@@ -7,11 +7,11 @@ If MATCH is non-nil, match all files against the regular
 expression.  If you want to match only against the file portion
 /<match>$ is recommended."
   (let (files)
-    (dolist (d (directory-files dir t "^[^.].*"))
-      (if (file-directory-p d)
-	  (setq files (append files (directory-files-recursive d match)))
-	(when (or (not match) (string-match match d))
-	  (setq files (append files (list d))))))
+    (dolist (d (directory-files-and-attributes dir t "^[^.].*"))
+      (if (eq (cadr d) t)
+	  (setq files (append files (directory-files-recursive (car d) match)))
+	(when (or (not match) (string-match match (car d)))
+	  (setq files (append files (list (car d)))))))
     files))
 
 ;;;###autoload
@@ -19,10 +19,10 @@ expression.  If you want to match only against the file portion
   "Return a list of directories in DIR recursively descending all
 subdirectories that do not start in a dot (.)."
   (let (dirs)
-    (dolist (d (directory-files dir t "^[^.].*"))
-      (when (file-directory-p d)
-	(setq dirs (append dirs (list d)))
-	(setq dirs (append dirs (directory-dirs-recursive d)))))
+    (dolist (d (directory-files-and-attributes dir t "^[^.].*"))
+      (when (eq (cadr d) t)
+	(setq dirs (append dirs (list (car d))
+			   (directory-dirs-recursive (car d))))))
     dirs))
 
 ;;;###autoload
@@ -30,10 +30,9 @@ subdirectories that do not start in a dot (.)."
   "Return a list of sub-directories in DIR. All dot directories
 are ignored. If NOSORT is non-nil then the list is not sorted."
   (let (dirlist)
-    ;; Note: directory-files-and-attributes
-    (dolist (subdir (directory-files dir t "^[^.].*" nosort))
-      (when (file-directory-p subdir)
-	(setq dirlist (append dirlist (list subdir)))))
+    (dolist (subdir (directory-files-and-attributes dir t "^[^.].*" nosort))
+      (when (eq (cadr subdir) t)
+	(setq dirlist (append dirlist (list (car subdir))))))
     dirlist))
 
 (provide 'dirfiles)
