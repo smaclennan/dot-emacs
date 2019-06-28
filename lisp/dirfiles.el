6023a0ea3ea2a1a7ec1c21baecb67451ac45d231
Sean MacLennan <seanm@seanm.ca>
2019-06-28 19:08:53 -0400

    Add directory-dirs-recursive

--- lisp/dirfiles.el
+++ lisp/dirfiles.el
@@ -16,6 +16,21 @@ match all files against the regular expression."
   (when match (setq match (concat "/" match)))
   (directory-files-recursive-intern dir match))
 
+(defun directory-dirs-recursive-intern (dir)
+  "Internal function for `directory-files-recursive'."
+  (let (dirs)
+    (dolist (d (directory-files dir t "^[^.].*"))
+      (when (file-directory-p d)
+	(setq dirs (append dirs (list d)))
+	(setq dirs (append dirs (directory-dirs-recursive-intern d)))))
+    dirs))
+
+;;;###autoload
+(defun directory-dirs-recursive (dir)
+  "Return a list of directories in DIR recursively descending all
+subdirectories that do not start in a dot (.)."
+  (directory-dirs-recursive-intern dir))
+
 ;;;###autoload
 (defun subdirs (dir &optional nosort)
   "Return a list of sub-directories in DIR. All dot directories
