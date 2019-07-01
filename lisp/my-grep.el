2a47b038b2bf62015d49f27ac86dd7d5222031f4
Sean MacLennan <seanm@seanm.ca>
2019-07-01 12:33:24 -0400

    Make f8 more like f6. Not sure if I am going to completely confuse myself, but let's try this. checkpatch was moved from C-f8 to C-f9.

--- lisp/my-grep.el
+++ lisp/my-grep.el
@@ -7,19 +7,22 @@
 (defvar my-grep-files-history nil
   "The minibuffer history list for `my-grep' and `my-grep-find's FILES argument.")
 
+(defun my-grep-exts ()
+  (let ((ext (if buffer-file-name (file-name-extension buffer-file-name))))
+    (if ext
+	;; This is my preference... and it is my code.
+	(if (or (string= ext "c") (string= ext "h"))
+	    "*.[ch]"
+	  (concat "*." ext))
+      "*")))
+
 (defun my-grep-interactive ()
   (let* ((word (current-word))
-	 (ext (if buffer-file-name (file-name-extension buffer-file-name)))
-	 (exts (if ext
-		   ;; This is my preference... and it is my code.
-		   (if (or (string= ext "c") (string= ext "h"))
-		       "*.[ch]"
-		     (concat "*." ext))
-		 "*"))
-	  (rprompt (concat "Regex [" word "]: "))
-	  (fprompt (concat "Files [" exts "]: ")))
-     (list (read-string rprompt nil 'my-grep-regex-history word)
-	   (read-string fprompt nil 'my-grep-files-history exts))))
+	 (exts (my-grep-exts))
+	 (rprompt (concat "Regex [" word "]: "))
+	 (fprompt (concat "Files [" exts "]: ")))
+    (list (read-string rprompt nil 'my-grep-regex-history word)
+	  (read-string fprompt nil 'my-grep-files-history exts))))
 
 (defun my-grep-doit(cmd)
   (when current-prefix-arg
@@ -28,26 +31,39 @@
 
 ;;;###autoload
 (defun my-grep (regex files)
-  "Run the Emacs `grep' command with some nicer defaults. The
-REGEX defaults to the word under the point. FILES defaults to the
-current extension.
+  "Run the Emacs `grep' command with some nicer defaults.
+
+The REGEX defaults to the word under the point. FILES defaults to
+the current extension.
 
 With a prefix arg you can edit the grep command before the grep
-is run. This allows extra flags like -i etc. Don't remove the -nH or
-`next-error' will not work."
+is run. This allows extra flags like -i etc. Don't remove the -nH
+or `next-error' will not work."
   (interactive (my-grep-interactive))
   (my-grep-doit (concat my-grep-prog " \"" regex "\" " files)))
 
 ;;;###autoload
+(defun my-grep-i-feel-lucky ()
+  "Run `my-grep' with no prompting.
+
+Ok, with a prefix arg you can edit the grep command before the
+grep is run."
+  (interactive)
+  (my-grep-doit
+   (concat my-grep-prog " \"" (current-word) "\" " (my-grep-exts))))
+
+;;;###autoload
 (defun my-grep-find (regex files)
-  "Run the Emacs `grep' command with some nicer defaults. The
-REGEX defaults to the word under the point. FILES defaults to the
-current extension. The unix find command is used to recursively
-find all files.
+  "Run the Emacs `grep-find' command with some nicer defaults.
+
+The REGEX defaults to the word under the point. FILES defaults to
+the current extension. The unix find command is used to
+recursively find all files.
 
 With a prefix arg you can edit the grep command before the grep
 is run. This allows extra flags like -i etc. It is generally a
-VBI (Very Bad Idea) to remove any of the existing flags."
+Very Bad Idea to remove any of the existing flags."
   (interactive (my-grep-interactive))
   (my-grep-doit (concat "find -name '" files "' -print0|"
-			"xargs -0 " my-grep-prog " '" regex "'")))
+			"xargs -0 " my-grep-prog " '"
+			regex "'")))
