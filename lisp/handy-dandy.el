7543d83286085335c9a32478bb84ac26e98439ab
Sean MacLennan <seanm@seanm.ca>
2019-07-03 23:19:38 -0400

    Tighten unixtime

--- lisp/handy-dandy.el
+++ lisp/handy-dandy.el
@@ -2,17 +2,8 @@
 
 ;;;###autoload
 (defun unixtime (seconds)
-  (interactive "sTime: ")
-  ;; Force it to a float for 32-bit systems.
-  (let ((time (string-to-number (concat seconds ".0"))))
-    (message "%s"
-	     (format-time-string
-	      "%a %b %d %T %Z %Y"
-	      ;; seconds-to-time from time-date.el in gnus
-	      (list (floor time 65536)
-		    (floor (mod time 65536))
-		    (floor (* (- time (ffloor time)) 1000000)))
-	      ))))
+  (interactive "dTime: ")
+  (message (format-time-string "%a %b %d %T %Z %Y" (seconds-to-time seconds))))
 
 ;;;###autoload
 (defun my-x-colour (number)
