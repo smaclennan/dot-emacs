d41c4e77269b71227563254ea0b549a1f321fada
Sean MacLennan <seanm@seanm.ca>
2019-07-10 20:23:22 -0400

    Add date to mail

--- rc/sendmail-rc.el
+++ rc/sendmail-rc.el
@@ -1,3 +1,10 @@
-;; I use doorknob just about everywhere... so default to sendmail
+;; I use doorknob everywhere... so default to sendmail
 (when (eq send-mail-function 'sendmail-query-once)
   (setq send-mail-function 'sendmail-send-it))
+
+;; For some reason Emacs does not add the date
+(defun mail-add-date ()
+  (save-excursion
+    (beginning-of-line)
+    (insert (format-time-string "Date: %a, %d %b %Y %T %z\n"))))
+(add-hook 'mail-setup-hook 'mail-add-date)
