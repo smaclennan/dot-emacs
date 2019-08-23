c827e318e76885ee7d90145cc5b523326b060b63
Sean MacLennan <seanm@seanm.ca>
2019-08-23 19:45:39 -0400

    Move find-file 'stuff' to files-rc.el

--- init.el
+++ init.el
@@ -169,27 +169,6 @@ the identifier."
 ;;; ------------------------------------------------------------
 ;;; Packages
 
-;; When was the last time you wanted to edit a .elc file?
-(defun my-find-file-predicate (file)
-  (not (string= (file-name-extension file) "elc")))
-
-(defun find-file-read-args (prompt mustmatch)
-  (list (read-file-name prompt nil default-directory mustmatch nil
-			'my-find-file-predicate)
-	t))
-
-(defvar commit-names '("COMMIT_EDITMSG" "svn-commit.tmp" "README")
-  "* List of commit buffer names.")
-
-(defun check-for-commit ()
-  "If this is a commit buffer, set to text mode."
-  (when (eq major-mode 'fundamental-mode)
-    (let ((buff (buffer-name)))
-      (dolist (name commit-names)
-	(when (string= buff name)
-	  (text-mode))))))
-(add-hook 'find-file-hooks 'check-for-commit t)
-
 (iswitchb-mode 1)
 (delete-selection-mode)
 
---
+++ rc/files-rc.el
@@ -0,0 +1,22 @@
+;; find-file is files
+
+;; When was the last time you wanted to edit a .elc file?
+(defun my-find-file-predicate (file)
+  (not (string= (file-name-extension file) "elc")))
+
+(defun find-file-read-args (prompt mustmatch)
+  (list (read-file-name prompt nil default-directory mustmatch nil
+			'my-find-file-predicate)
+	t))
+
+(defvar commit-names '("COMMIT_EDITMSG" "svn-commit.tmp" "README")
+  "* List of commit buffer names.")
+
+(defun check-for-commit ()
+  "If this is a commit buffer, set to text mode."
+  (when (eq major-mode 'fundamental-mode)
+    (let ((buff (buffer-name)))
+      (dolist (name commit-names)
+	(when (string= buff name)
+	  (text-mode))))))
+(add-hook 'find-file-hooks 'check-for-commit t)
