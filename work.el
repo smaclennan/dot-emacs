4a15aff659b269159215b17163ab2ae339d022a0
Sean MacLennan <seanm@seanm.ca>
2019-09-02 12:05:33 -0400

    Remove last remnants of QNX_BASE_DIR.

--- work.el
+++ work.el
@@ -1,8 +1,2 @@
-(defun qnx-load-qnx ()
-  (interactive)
-  (when (getenv "QNX_SANDBOX")
-    (let ((base (getenv "QNX_BASE_DIR")))
-      (unless base (setq base "~/work"))
-      (load (concat (file-name-as-directory base) "emacs/qnx")))))
-
-(qnx-load-qnx)
+(when (getenv "QNX_SANDBOX")
+  (load "~/work/emacs/qnx" t))
