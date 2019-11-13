99ddbe72d11907a7afefe696cbec6962ad0c4436
Sean MacLennan <seanm@seanm.ca>
2019-11-12 21:04:22 -0500

    More efficient for newer kernels and more portable to old kernels/emacs

--- sys/linux.el
+++ sys/linux.el
@@ -2,10 +2,12 @@
 (defun sys-nproc ()
   "Return number of cpu devices."
   (setq sys-nproc
-	(if (file-exists-p "/sys/bus/cpu/devices")
+	(condition-case nil
 	    (length (directory-files "/sys/bus/cpu/devices" nil "cpu[0-9]*"))
-	  (with-current-buffer (find-file-noselect "/proc/cpuinfo")
-	    (count-matches "^processor" (point-min))))))
+	  (error
+	   (with-temp-buffer
+	     (insert-file-contents "/proc/cpuinfo")
+	     (count-matches "^processor" (point-min) (point-max)))))))
 
 ;;;###autoload
 (defun sys-mem ()
@@ -16,8 +18,10 @@
       (goto-char (point-min))
       (re-search-forward "^Memtotal: *\\([0-9]+\\) kB$")
       (setq sys-mem (* (string-to-number (match-string 1)) 1024))
-      (re-search-forward "^Memavailable: *\\([0-9]+\\) kB$")
-      (setq free (* (string-to-number (match-string 1)) 1024)))
+      (if (re-search-forward "^Memavailable: *\\([0-9]+\\) kB$" nil t)
+	  (setq free (* (string-to-number (match-string 1)) 1024))
+	(re-search-forward "^Memfree: *\\([0-9]+\\) kB$" nil t)
+	(setq free (* (string-to-number (match-string 1)) 1024))))
     (list sys-mem free)))
 
 (defun cpuinfo-find (field)
