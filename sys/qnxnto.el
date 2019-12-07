b01f86511a3241396eb2a57947076eaf6df9c12c
Sean MacLennan <seanm@seanm.ca>
2019-12-06 20:53:51 -0500

    Optimize the pidin call

--- sys/qnxnto.el
+++ sys/qnxnto.el
@@ -1,19 +1,19 @@
+(defun do-pidin-info ()
+  (with-temp-buffer
+    (call-process "pidin" nil t nil "info")
+    (re-search-backward "Freemem:[0-9]+MB/\\([0-9]+\\)MB")
+    (setq sys-mem (* (string-to-number (match-string 1)) #x100000))
+    (count-matches "^Processor")))
+
 ;;;###autoload
 (defun sys-nproc ()
   "Return the number of processors reported by pidin."
-  (with-temp-buffer
-    (call-process "pidin" nil t nil "info")
-    (count-matches "^Processor" (point-min))))
+  (if sys-nproc sys-nproc (do-pidin-info)))
 
 ;;;###autoload
 (defun sys-mem ()
   "Return the total and free memory."
-  (unless sys-mem
-    ;; First time we need to call pidin to get total mem
-    (with-temp-buffer
-      (call-process "pidin" nil t nil "info")
-      (re-search-backward "Freemem:[0-9]+MB/\\([0-9]+\\)MB")
-      (setq sys-mem (* (string-to-number (match-string 1)) #x100000))))
+  (unless sys-mem (do-pidin-info))
   (list sys-mem
 	;; Yes ls -ld /proc = free memory
 	(nth 7 (file-attributes "/proc"))))
