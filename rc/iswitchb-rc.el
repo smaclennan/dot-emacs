6fa98ca2fa3ba534f4fe74d9dd2e6ea28a9dbb8e
Sean MacLennan <seanm@seanm.ca>
2019-12-22 14:57:31 -0500

    Use the arrow keys to scroll through the buffers in iswitchb

---
+++ rc/iswitchb-rc.el
@@ -0,0 +1,8 @@
+;; You cannot just add key definitions to `iswitchb-mode-map' since
+;; `iswitchb-read-buffer' creates a new map every time it is called.
+;; So instead you add to this hook that is run after the new map is
+;; created.
+(add-hook 'iswitchb-define-mode-map-hook
+	  (lambda ()
+	    (define-key iswitchb-mode-map [right] 'iswitchb-next-match)
+	    (define-key iswitchb-mode-map [left]  'iswitchb-prev-match)))
