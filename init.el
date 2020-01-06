527e12ef5185d3d035ced0b84fa46d64b8eb78fc
Sean MacLennan <seanm@seanm.ca>
2020-01-05 20:26:08 -0500

    Now that we have startup-rc.el... move startup code there.

--- init.el
+++ init.el
@@ -52,14 +52,6 @@
 
 (delete-selection-mode)
 
-;; Collect the auto-save and backup files in one place.
-;; I added the following to my crontab:
-;; 17 5 * * * find $HOME/.autosave -mtime +7 -delete
-(make-directory "~/.autosave" t)
-(setq auto-save-list-file-prefix nil) ;; don't create auto-save-list directory
-(setq auto-save-file-name-transforms `((".*" "~/.autosave/" t)))
-(setq backup-directory-alist '((".*" . "~/.autosave")))
-
 ;;; ------------------------------------------------------------
 ;;; Keys
 
--- rc/startup-rc.el
+++ rc/startup-rc.el
@@ -2,6 +2,15 @@
       inhibit-startup-screen t
       initial-scratch-message ";; This buffer is for goofing around in.\n\n")
 
+;; Collect the auto-save and backup files in one place.
+;; I added the following to my crontab:
+;; 17 5 * * * find $HOME/.autosave -mtime +7 -delete
+(make-directory "~/.autosave" t)
+(setq auto-save-list-file-prefix nil) ;; don't create auto-save-list directory
+(setq auto-save-file-name-transforms `((".*" "~/.autosave/" t)))
+;; Not really part of startup... but belongs with auto-save
+(setq backup-directory-alist '((".*" . "~/.autosave")))
+
 (require 'server)
 
 (defun display-startup-echo-area-message ()
