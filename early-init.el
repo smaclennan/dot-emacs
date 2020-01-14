6330173c26d26021c943c88c865e2c972c648b8f
Sean MacLennan <seanm@seanm.ca>
2020-01-13 23:24:54 -0500

    More speedup. Especially if you don't have any packages.

--- early-init.el
+++ early-init.el
@@ -1,4 +1,6 @@
-;; early-init.el is new in 27.1. This shaves a good 0.15 seconds off the init time.
+;; early-init.el is new in 27.1. This shaves a good 0.250 seconds off the init time.
 
 (tool-bar-mode 0)
 (menu-bar-mode 0)
+
+(setq package-enable-at-startup nil)
