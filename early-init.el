b99a3576e5864d21d47244e3721df88c44aa22a7
Sean MacLennan <seanm@seanm.ca>
2020-02-03 19:50:53 -0500

    tool-bar-mode is window system only

--- early-init.el
+++ early-init.el
@@ -1,6 +1,6 @@
 ;; early-init.el is new in 27.1. This shaves a good 0.250 seconds off the init time.
 
-(tool-bar-mode 0)
+(when window-system (tool-bar-mode 0))
 (menu-bar-mode 0)
 
 (setq package-enable-at-startup nil)
