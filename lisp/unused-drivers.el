ac321cabe0e55adfe9f22d2760bd052383c742d9
Sean MacLennan <seanm@seanm.ca>
2020-02-22 15:47:28 -0500

    List unused drivers for Linux only

---
+++ lisp/unused-drivers.el
@@ -0,0 +1,69 @@
+;; Very Linux specific
+;; Hint to self: Self, you cannot scp /proc/modules.
+
+(defvar module-whitelist nil
+  "*Vetted modules to whitelist.
+NOTE: They must have underscores (_) not dashes (-)!")
+
+(defvar lsmod-file nil
+  "*File containing list of modules.
+This is mainly for vetting embedded computers. If nil, defaults
+to /proc/modules.")
+
+(defun build-drivers-list (release)
+  (with-temp-buffer
+    (let ((moddep (concat "/lib/modules/" release "/modules.dep"))
+	  dlist lines)
+      (insert-file-contents moddep nil nil nil t)
+      (setq lines (count-lines (point-min) (point-max)))
+      (goto-char (point-min))
+      (while (re-search-forward ".*/\\([A-Za-z0-9_-]+\\)\.ko:.*$" nil t)
+	;; Get the basename and convert - to _
+	(setq dlist (cons (replace-regexp-in-string "-" "_" (match-string 1) nil t) dlist)))
+      ;; Sanity check
+      (unless (eq lines (length dlist)) (error "DRIVERS MISMATCH: lines %d list %d" lines (length dlist)))
+    dlist)))
+
+(defun build-lsmod-list ()
+  (let ((file (if lsmod-file lsmod-file "/proc/modules")))
+    (with-temp-buffer
+      (let (llist lines)
+	(insert-file-contents file nil nil nil t)
+	(goto-char (point-min))
+	(when (looking-at "Module") (kill-whole-line))
+	(setq lines (count-lines (point-min) (point-max)))
+	(while (re-search-forward "^\\([^ ]+\\) .*" nil t)
+	  (setq llist (cons (match-string 1) llist)))
+	;; Sanity check
+	(unless (eq lines (length llist)) (error "LSMOD MISMATCH: lines %d list %d" lines (length llist)))
+	llist))))
+
+;;;###autoload
+(defun unused-drivers (release)
+  (interactive (list
+		(let ((rel (uname "-r")))
+		  (read-string (format "Release [%s]: " rel) nil nil rel))))
+  (let ((used-list (build-lsmod-list))
+	(driver-list (build-drivers-list release))
+	dcount ucount fcount)
+    (setq dcount (length driver-list) ucount (length used-list))
+    (dolist (mod used-list)
+      (setq driver-list (delete mod driver-list)))
+    (setq fcount (length driver-list))
+    ;; Sanity check
+    (unless (eq dcount (+ ucount fcount))
+      (error "dcount %d != ucount %d + fcount %d" dcount ucount fcount))
+
+    ;; Remove the whitelisted modules - do this after counts
+    (dolist (mod module-whitelist)
+      (setq driver-list (delete mod driver-list)))
+
+    (with-current-buffer (get-buffer-create "*driver list*")
+      (erase-buffer)
+      (dolist (mod driver-list)
+	(insert (concat mod "\n")))
+      (insert (format "\n%d unused drivers\n" (length driver-list))))
+
+    (if driver-list
+	(display-buffer "*driver list*")
+      (message "No unused drivers."))))
