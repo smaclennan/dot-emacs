9983b14dc12c637989230a65fe58ccebb4a97f4f
Sean MacLennan <seanm@seanm.ca>
2020-02-26 19:49:50 -0500

    Small cleanups

--- lisp/my-c-helpers.el
+++ lisp/my-c-helpers.el
@@ -236,12 +236,4 @@ spaces. Biased towards tabs."
 		  (if (and (> spaces 0) (> tabs 0)) " (mixed)")))))
     (>= tabs spaces)))
 
-;;;###autoload
-(defun xref-find-definitions-prompt ()
-  "Same as `xref-find-definitions' except it always prompts for
-the identifier."
-  (interactive)
-  (let ((current-prefix-arg t))
-    (call-interactively 'xref-find-definitions)))
-
 (provide 'my-c-helpers)
--- rc/bindings-rc.el
+++ rc/bindings-rc.el
@@ -84,3 +84,10 @@
   "Interactive version of `buffer-file-name'. Useful on console."
   (interactive)
   (message "%s" (abbreviate-file-name buffer-file-name)))
+
+(defun xref-find-definitions-prompt ()
+  "Same as `xref-find-definitions' except it always prompts for
+the identifier."
+  (interactive)
+  (let ((current-prefix-arg t))
+    (call-interactively 'xref-find-definitions)))
