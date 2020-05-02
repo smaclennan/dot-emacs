503ce09731cada03b73d3a9a3f6185bf6aa256cf
Sean MacLennan <seanm@seanm.ca>
2020-05-02 10:10:24 -0400

    Prefer smu for markdown generation

--- rc/markdown-mode-rc.el
+++ rc/markdown-mode-rc.el
@@ -1,1 +1,7 @@
-(setq markdown-command "markdown_py")
+(cond
+ ((executable-find "smu")
+  ;; https://github.com/karlb/smu
+  (setq markdown-command "smu"))
+ ((executable-find "markdown_py")
+  (setq markdown-command "markdown_py"))
+ (t (message "Warning: no markdown program found")))
