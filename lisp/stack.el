4eea191477cfc391b836604007a66016da0cf671
Sean MacLennan <smaclennan@blackberry.com>
2021-08-22 15:29:38 -0400

    Document the stack design

--- lisp/stack.el
+++ lisp/stack.el
@@ -1,5 +1,17 @@
 ;; LIFO stack macros
 
+;; At the cost of a sentinel ('STACK) at the start of the stack we can
+;; create a very efficient push/pop that has no branches. Because we
+;; are always using setcdr to a non-empty stack, we never need to
+;; assign back to the stack variable.
+
+;; We always push and pop from the end of the list. When we get down
+;; to just the sentinel, the `cdr' in `stack-pop' will return nil
+;; `car-safe' will return nil. `car-safe' is slightly more efficient
+;; than `car'.
+
+;; `stack-clear' guarantees that the sentinel is kept on the list.
+
 (defmacro stack-create ()
   "Create an empty lifo stack."
   `(cons 'STACK nil))
