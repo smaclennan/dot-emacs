d0d017f50d033fa09a57d459d73b579dbac1eff3
Sean MacLennan <seanm@seanm.ca>
2021-08-24 15:40:12 -0400

    asm-mode has way too many symbol constituents

--- rc/asm-mode-rc.el
+++ rc/asm-mode-rc.el
@@ -1,1 +1,7 @@
 (add-hook 'asm-mode-hook 'my-compile-command)
+
+;; We only want _ as a symbol constituent
+(with-syntax-table asm-mode-syntax-table
+  (cl-loop for char from ?! to ?~ unless (eq char ?_) do
+	   (when (eq ?_ (char-syntax char))
+	     (modify-syntax-entry char "." ))))
