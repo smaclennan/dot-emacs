609db46ebc0f64361bcebc87bd282073e109492f
Sean MacLennan <smaclennan@blackberry.com>
2021-08-25 20:31:20 -0400

    Much better version

--- rc/asm-mode-rc.el
+++ rc/asm-mode-rc.el
@@ -6,17 +6,6 @@
 	   (when (eq ?_ (char-syntax char))
 	     (modify-syntax-entry char "." ))))
 
-;; This should be the `asm-calculate-indention' function with the
-;; addition of dot commands
-(defun asm-calculate-indentation ()
-  (or
-   ;; Flush labels to the left margin.
-   (and (looking-at "\\(\\sw\\|\\s_\\)+:") 0)
-   ;; Same thing for `;;;' comments.
-   (and (looking-at "\\s<\\s<\\s<") 0)
-   ;; SAM Same thing for dot commands
-   (and (looking-at "\\(\\s_\\)*\\.") 0)
-   ;; Simple `;' comments go to the comment-column.
-   (and (looking-at "\\s<\\(\\S<\\|\\'\\)") comment-column)
-   ;; The rest goes at the first tab stop.
-   (indent-next-tab-stop 0)))
+;; .if/.else/.endif left justified
+(advice-add 'asm-calculate-indentation :filter-return
+	    (lambda(x) (if (looking-at "\\(\\s_\\)*\\.\\(if\\|else\\|endif\\)") 0 x)))
