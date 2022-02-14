65badf8233b5595ad1e503dd141039bc41ab1bd1
Sean MacLennan <seanm@seanm.ca>
2022-02-14 14:17:23 -0500

    Doc dwm and remove dead code

--- rc/ediff-rc.el
+++ rc/ediff-rc.el
@@ -1,3 +1,9 @@
+;; If you use dwm and tabbed Emacs, you want to make the ediff frame floating:
+;; Add to rules[]
+;; { "Emacs",    "Ediff",    NULL,       0,            1,           -1 },
+;; gtk, of course, does not give the proper instance name because it
+;; knows better than you. So for dwm build an X version of Emacs.
+
 (defun my-ediff-quit ()
   "Ediff seems to always leave the bottom buffer selected but I
 want the top buffer."
@@ -5,10 +11,3 @@ want the top buffer."
     (while (not (eq (selected-window) top-window))
       (other-window 1))))
 (add-hook 'ediff-quit-hook 'my-ediff-quit t)
-
-;; -------------------
-;; Hack to put the ediff control in the right place. If we don't do
-;; this then the window loses focus under dwm.
-;; SAM do we still need this?
-;; SAM (setf (alist-get 'top ediff-control-frame-parameters) 1)
-;; SAM (setf (alist-get 'left ediff-control-frame-parameters) 856)
