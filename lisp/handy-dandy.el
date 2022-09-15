ea1e5a163be17561128311831a87511c4e99c646
Sean MacLennan <seanm@seanm.ca>
2022-09-15 12:56:54 -0400

    Revert "Install ws-butler as a package"
    This reverts commit 5824dff54c39384468b34173a6102cc14b7836b7.

--- lisp/handy-dandy.el
+++ lisp/handy-dandy.el
@@ -38,7 +38,8 @@ An ARG comments out the old line."
       (insert line "\n"))))
 
 (defvar mine-not
-  (concat "/elpa/\\|rcfiles.el\\|custom.el\\|-loaddefs\\|view-kill.el"))
+  (concat "/elpa/\\|/ws-butler.el\\|rcfiles.el\\|custom.el\\|"
+	  "-loaddefs\\|view-kill.el"))
 
 (defun mine (&optional all)
   "Return a list of all my .el files.
