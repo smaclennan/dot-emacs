fe0930c9b4abb8b7e2cdea5211084e4fa5dce49c
Sean MacLennan <seanm@seanm.ca>
2023-02-09 19:20:40 -0500

    The essiential C-x M-c M-butterfly command

---
+++ lisp/butterfly.el
@@ -0,0 +1,15 @@
+(defun butterfly ()
+  (interactive)
+  (unless (and
+	   (equal (read-key-sequence "C-x M-c M-b") "u")
+	   (equal (read-key-sequence "C-x M-c M-bu") "t")
+	   (equal (read-key-sequence "C-x M-c M-but") "t")
+	   (equal (read-key-sequence "C-x M-c M-butt") "e")
+	   (equal (read-key-sequence "C-x M-c M-butte") "r")
+	   (equal (read-key-sequence "C-x M-c M-butter") "f")
+	   (equal (read-key-sequence "C-x M-c M-butterf") "l")
+	   (equal (read-key-sequence "C-x M-c M-butterfl") "y")
+	   (message "I hope you know what you are doing"))
+    (error "Loser")))
+
+(global-set-key (kbd "C-x M-c M-b") `butterfly)
