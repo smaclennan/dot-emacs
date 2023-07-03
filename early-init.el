05b957916ad9c99fbc99e3d9e223fa1e03dcc48e
Sean MacLennan <seanm@seanm.ca>
2023-07-03 15:04:25 -0400

    Add a comment about emacs-init-time

--- early-init.el
+++ early-init.el
@@ -1,5 +1,6 @@
 ;; -*-no-byte-compile: t -*-
 ;; early-init.el is new in 27.1. This shaves a good 1/4 second off the init time.
+;; emacs-init-time
 
 (when window-system (tool-bar-mode 0))
 (menu-bar-mode 0)
