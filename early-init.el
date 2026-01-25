1869c0dcff8afd9cf9c22e821e00f3ce7140f93a
Sean MacLennan <smaclennan@blackberry.com>
2026-01-25 18:57:44 -0500

    Need x-is-remote hack

--- early-init.el
+++ early-init.el
@@ -8,3 +8,11 @@
 (setq package-enable-at-startup nil)
 
 (setq warning-suppress-log-types '((files missing-lexbind-cookie)))
+
+;; This needs to be very early and needs my `x-is-remote' hack for
+;; slow X forwarding connections
+;; Note: x-display-name not set yet
+(let ((x-display-name (getenv "DISPLAY")))
+  (and (boundp 'x-is-remote)
+       (string-match "[^:]+:" x-display-name)
+       (setq x-is-remote t)))
