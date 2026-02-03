58dc33bd54b7f7ff20bcafbd25966517fd59e789
Sean MacLennan <smaclennan@blackberry.com>
2026-02-02 21:37:31 -0500

    Got weather working again

--- lisp/weather.el
+++ lisp/weather.el
@@ -1,6 +1,6 @@
 ;; -*- lexical-binding: t -*-
 
-(defvar weather-url "https://weather.gc.ca/city/pages/on-118_metric_e.html"
+(defvar weather-url "https://weather.gc.ca/en/location/index.html?coords=45.403,-75.687"
   "The url for the weather. Must be weather.gc.ca.")
 
 (defvar weather-fancy t
@@ -10,14 +10,17 @@ Only works for X11.")
 ;;;###autoload
 (defun weather ()
   (interactive)
-  (let (gif tempc tempf conditions giffile)
+  (let (time gif tempc tempf conditions giffile)
     (with-current-buffer (url-retrieve-synchronously weather-url t)
       (goto-char (point-min))
+      (if (re-search-forward "<time datetime=\"\\([0-9TZ:.-]+\\)" nil t)
+	  (setq time (format-time-string "%l:%M %p"
+					 (date-to-time (match-string 1))))
+	(setq time "n/a"))
       (re-search-forward "src=\"/\\(weathericons/[0-9]+\\.gif\\)\" alt=\"\\([^\"]+\\)")
       (setq gif (match-string 1))
       (setq conditions (match-string 2))
-      (re-search-forward "mrgn-bttm-sm lead mrgn-tp-sm")
-      (re-search-forward "<span data-v-[0-9a-f]+>\\([0-9]+\\)")
+      (re-search-forward "<span data-v-[0-9a-f]+>\\(-?[0-9]+\\)")
       (setq tempc (match-string 1)))
 
     (setq tempf (+ (/ (* (string-to-number tempc) 9) 5) 32))
@@ -38,7 +41,8 @@ Only works for X11.")
 	  (with-current-buffer (get-buffer-create "*weather*")
 	    (erase-buffer)
 	    (insert-image (create-image giffile))
-	    (insert " " conditions " " tempc "\u00b0C " tempf "\u00b0F\n"))
+	    (insert " " conditions " " tempc "\u00b0C " tempf "\u00b0F\n"
+		    " Last updated " time "\n"))
 
 	  (display-buffer-at-bottom (get-buffer "*weather*") '((window-height . 6))))
-	(message "%s  %sC  %sF" conditions tempc tempf))))
+	(message "%s  %sC  %sF  Updated %s" conditions tempc tempf time))))
