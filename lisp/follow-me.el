c4714bf4eee2d148f365b1afd54f11a4619af4c1
Sean MacLennan <smaclennan@blackberry.com>
2026-03-12 22:43:44 -0400

    Fancier follow-me

--- lisp/follow-me.el
+++ lisp/follow-me.el
@@ -5,14 +5,21 @@
   :lighter " fm!"
   :keymap '(([mouse-1] . follow-me)))
 
+;;;###autoload
 (defun follow-me (_event)
   (interactive "e")
   (browse-url (thing-at-point 'url)))
 
 ;;;###autoload
-(defun follow-me-add (begin end)
+(defun follow-me-add (begin end &optional display)
   (interactive "r")
-  (put-text-property begin end 'face '(:foreground "blue"))
+  (put-text-property begin end 'face '(:foreground "blue" :underline nil))
+  (put-text-property begin end 'mouse-face '(:foreground "blue" :underline t))
+  (put-text-property begin end 'display display)
+  ;; pointer
+  ;; display
   (put-text-property begin end 'keymap follow-me-mode-map))
+
+;; http://seanm.ca/
 
 (provide 'follow-me)
